75ab46024915c7bc735fb6aee6473d65ca8313f7
Alexander Fedorov <alexander.fedorov@arsysop.ru>
2019-03-18 12:13:55 +0300

    Bug 545477 - [Passage] split org.eclipse.passage.lic.model.ui bundle
    + org.eclipse.passage.lic.features.e4.ui
    + org.eclipse.passage.lic.licenses.e4.ui
    + org.eclipse.passage.lic.products.e4.ui
    + org.eclipse.passage.lic.users.e4.ui
    - org.eclipse.passage.lic.model.ui
    
    Change-Id: I393770261eecbd6aa2ea033a6c42848e71890150
    Signed-off-by: Alexander Fedorov <alexander.fedorov@arsysop.ru>

---
+++ bundles/org.eclipse.passage.lic.features.e4.ui/src/org/eclipse/passage/lic/internal/features/e4/ui/FeaturesE4UiProcessor.java
@@ -0,0 +1,51 @@
+/*******************************************************************************
+ * Copyright (c) 2018-2019 ArSysOp
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     ArSysOp - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.passage.lic.internal.features.e4.ui;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.e4.core.contexts.IEclipseContext;
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.passage.lic.features.edit.FeaturesEditPlugin;
+import org.eclipse.passage.lic.features.model.meta.FeaturesPackage;
+import org.eclipse.passage.lic.jface.resource.LicensingImages;
+import org.eclipse.swt.widgets.Display;
+
+public class FeaturesE4UiProcessor {
+
+	@Execute
+	void process(IEclipseContext context) {
+		Display display = context.get(Display.class);
+		if (display != null) {
+			doRegisterImages();
+		}
+	}
+
+	private void doRegisterImages() {
+		LicensingImages.getImageRegistry();
+		String pattern = "$nl$/icons/full/obj16/%s"; //$NON-NLS-1$
+
+		registerFeatures(pattern);
+	}
+
+	private void registerFeatures(String pattern) {
+		Map<String, String> paths = new HashMap<String, String>();
+		FeaturesPackage features = FeaturesPackage.eINSTANCE;
+		paths.put(features.getFeatureSet().getName(), String.format(pattern, "feature.png")); //$NON-NLS-1$
+		paths.put(features.getFeature().getName(), String.format(pattern, "feature.png")); //$NON-NLS-1$
+		paths.put(features.getFeatureVersion().getName(), String.format(pattern, "feature.png")); //$NON-NLS-1$
+		LicensingImages.declareImages(FeaturesEditPlugin.class, paths);
+	}
+
+}
---
+++ bundles/org.eclipse.passage.lic.users.e4.ui/src/org/eclipse/passage/lic/internal/users/e4/ui/UsersE4UiProcessor.java
@@ -0,0 +1,50 @@
+/*******************************************************************************
+ * Copyright (c) 2018-2019 ArSysOp
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     ArSysOp - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.passage.lic.internal.users.e4.ui;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.e4.core.contexts.IEclipseContext;
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.passage.lic.jface.resource.LicensingImages;
+import org.eclipse.passage.lic.users.edit.UsersEditPlugin;
+import org.eclipse.passage.lic.users.model.meta.UsersPackage;
+import org.eclipse.swt.widgets.Display;
+
+public class UsersE4UiProcessor {
+
+	@Execute
+	void process(IEclipseContext context) {
+		Display display = context.get(Display.class);
+		if (display != null) {
+			doRegisterImages();
+		}
+	}
+
+	private void doRegisterImages() {
+		LicensingImages.getImageRegistry();
+		String pattern = "$nl$/icons/full/obj16/%s"; //$NON-NLS-1$
+
+		registerUsers(pattern);
+	}
+
+	private void registerUsers(String pattern) {
+		Map<String, String> paths = new HashMap<String, String>();
+		UsersPackage users = UsersPackage.eINSTANCE;
+		paths.put(users.getUserOrigin().getName(), String.format(pattern, "user.png")); //$NON-NLS-1$
+		paths.put(users.getUser().getName(), String.format(pattern, "user.png")); //$NON-NLS-1$
+		LicensingImages.declareImages(UsersEditPlugin.class, paths);
+	}
+
+}
