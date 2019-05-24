63405e2c4a5e45236498678b2d2a465fbbd1ee04
Alexander Fedorov <alexander.fedorov@arsysop.ru>
2019-05-24 12:58:53 +0300

    Bug 547609 - [Passage] LicensingAction should respect RestrictionVerdict
    Change-Id: Iecbd849f70de1d6a82c8a184c43851195be15100
    Signed-off-by: Alexander Fedorov <alexander.fedorov@arsysop.ru>

--- bundles/org.eclipse.passage.lic.jface/src/org/eclipse/passage/lic/jface/actions/LicensedAction.java
+++ bundles/org.eclipse.passage.lic.jface/src/org/eclipse/passage/lic/jface/actions/LicensedAction.java
@@ -12,9 +12,15 @@
  *******************************************************************************/
 package org.eclipse.passage.lic.jface.actions;
 
-import org.eclipse.core.runtime.IStatus;
+import java.util.Iterator;
+
 import org.eclipse.jface.action.Action;
-import org.eclipse.passage.lic.jface.LicensingWidgets;
+import org.eclipse.passage.lic.api.requirements.LicensingRequirement;
+import org.eclipse.passage.lic.api.restrictions.RestrictionVerdict;
+import org.eclipse.passage.lic.base.restrictions.RestrictionVerdicts;
+import org.eclipse.passage.lic.equinox.requirements.EquinoxRequirements;
+import org.eclipse.passage.lic.equinox.restrictions.EquinoxRestrictions;
+import org.eclipse.passage.lic.jface.dialogs.LicensingStatusDialog;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Shell;
 
@@ -23,9 +29,19 @@ public class LicensedAction extends Action {
 	@Override
 	public void runWithEvent(Event event) {
 		Shell shell = event.display.getActiveShell();
-		IStatus validated = LicensingWidgets.validateLicense(shell, getId());
-		if (!validated.isOK()) {
-			return;
+		String featureId = getId();
+		Iterable<LicensingRequirement> required = EquinoxRequirements.getFeatureRequirements(featureId);
+		Iterator<LicensingRequirement> iterator = required.iterator();
+		if (iterator.hasNext()) {
+			Iterable<RestrictionVerdict> verdicts = EquinoxRestrictions.getFeatureVerdicts(featureId);
+			if (RestrictionVerdicts.shouldPauseExecution(RestrictionVerdicts.resolveLastVerdict(verdicts))) {
+				LicensingStatusDialog dialog = new LicensingStatusDialog(shell, featureId);
+				dialog.open();
+				verdicts = EquinoxRestrictions.getFeatureVerdicts(featureId);
+			}
+			if (RestrictionVerdicts.shouldInterruptExecution(RestrictionVerdicts.resolveLastVerdict(verdicts))) {
+				return;
+			}
 		}
 		super.runWithEvent(event);
 	}
