f149f3cd12e1d0b43f65043bcbc0ce46ed91cbdb
Alexander Fedorov <alexander.fedorov@arsysop.ru>
2019-06-05 16:46:51 +0300

    Bug 547655 - [Passage] provide an "Issue License" wizard
    Add date selection dialog
    
    Change-Id: If25fa17d8fd2dc7ba9942591f53afec978199dbf
    Signed-off-by: Alexander Fedorov <alexander.fedorov@arsysop.ru>

---
+++ bundles/org.eclipse.passage.loc.workbench/src/org/eclipse/passage/loc/jface/dialogs/DateDialog.java
@@ -0,0 +1,66 @@
+/*******************************************************************************
+ * Copyright (c) 2019 ArSysOp
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     ArSysOp - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.passage.loc.jface.dialogs;
+
+import java.time.LocalDate;
+
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.DateTime;
+import org.eclipse.swt.widgets.Shell;
+
+public class DateDialog extends Dialog {
+
+	private DateTime calendar;
+	private LocalDate selected;
+
+	public DateDialog(Shell shell, LocalDate initial) {
+		super(shell);
+		this.selected = initial != null ? initial : LocalDate.now();
+	}
+
+	@Override
+	protected Control createDialogArea(Composite parent) {
+		Composite area = (Composite) super.createDialogArea(parent);
+		calendar = new DateTime(area, SWT.CALENDAR | SWT.BORDER);
+		showLocalDate(selected);
+		return area;
+	}
+
+	protected LocalDate collectLocalDateTime() {
+		int year = calendar.getYear();
+		int month = calendar.getMonth();
+		int day = calendar.getDay();
+		return LocalDate.of(year, month, day);
+	}
+
+	protected void showLocalDate(LocalDate localDate) {
+		int year = localDate.getYear();
+		int month = localDate.getMonthValue();
+		int day = localDate.getDayOfMonth();
+		calendar.setDate(year, month, day);
+	}
+
+	@Override
+	protected void okPressed() {
+		selected = collectLocalDateTime();
+		super.okPressed();
+	}
+
+	public LocalDate getSelected() {
+		return selected;
+	}
+
+}
