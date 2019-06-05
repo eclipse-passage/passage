deff927fe06bab260ba62d1d5f26bce31576f228
Alexander Fedorov <alexander.fedorov@arsysop.ru>
2019-06-05 11:55:30 +0300

    Bug 547655 - [Passage] provide an "Issue License" wizard
    Increase size for "edit" dialogs
    
    Change-Id: I9e17e302d8c6adf7ddb2fcb9595859a9c51739f2

---
+++ bundles/org.eclipse.passage.loc.workbench.emfforms/src/org/eclipse/passage/loc/internal/workbench/emfforms/OperatorDialogWrapper.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2019 ArSysOp
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     ArSysOp - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.passage.loc.internal.workbench.emfforms;
+
+import org.eclipse.emf.ecp.edit.internal.swt.util.DialogWrapper;
+import org.eclipse.emf.ecp.edit.spi.swt.util.ECPDialogExecutor;
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.swt.widgets.Shell;
+
+@SuppressWarnings("restriction")
+public class OperatorDialogWrapper implements DialogWrapper {
+
+	@Override
+	public void openDialog(Dialog dialog, ECPDialogExecutor callBack) {
+		dialog.create();
+		Shell shell = dialog.getShell();
+		shell.setSize(Math.max(900, shell.getSize().x), Math.max(500, shell.getSize().y));
+		callBack.handleResult(dialog.open());
+	}
+
+}
