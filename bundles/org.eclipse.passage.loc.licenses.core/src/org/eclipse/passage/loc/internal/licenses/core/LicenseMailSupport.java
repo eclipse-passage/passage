582dd4400c663685d214fde34e008f930e497c4c
Sergei Kovalchuk <sergei.kovalchuk@nxp.com>
2019-09-16 16:44:50 +0300

    Bug 550956 - [Passage] Support mail attachment
    The eml file creating feature provided from wizard license detailed
    page.
    
    Currently supported two mail option:
    - mail body preparation
    - .eml file preparation (with attachment)

--- bundles/org.eclipse.passage.loc.licenses.core/src/org/eclipse/passage/loc/internal/licenses/core/LicenseMailSupport.java
+++ bundles/org.eclipse.passage.loc.licenses.core/src/org/eclipse/passage/loc/internal/licenses/core/LicenseMailSupport.java
@@ -12,21 +12,40 @@
  *******************************************************************************/
 package org.eclipse.passage.loc.internal.licenses.core;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
 import org.eclipse.osgi.util.NLS;
 import org.eclipse.passage.lic.licenses.LicenseGrantDescriptor;
 import org.eclipse.passage.lic.licenses.LicensePackDescriptor;
 import org.eclipse.passage.loc.internal.licenses.core.i18n.LicensesCoreMessages;
 
-public class LicenseMail {
+public class LicenseMailSupport {
 
 	private static final String MAILTO = "mailto:"; //$NON-NLS-1$
 	private static final String MAILTO_SUBJECT = "?subject="; //$NON-NLS-1$
 	private static final String MAILTO_BODY = "&body="; //$NON-NLS-1$
 	private static final String MAILTO_SEPARATOR = "%0A"; //$NON-NLS-1$
+	private static final String MAIL_EML_EXTENSION = ".eml"; //$NON-NLS-1$
 
 	private final LicensePackDescriptor licensePack;
 
-	public LicenseMail(LicensePackDescriptor licensePack) {
+	public LicenseMailSupport(LicensePackDescriptor licensePack) {
 		this.licensePack = licensePack;
 	}
 
@@ -70,7 +89,7 @@ public class LicenseMail {
 		return builder.toString();
 	}
 
-	public String getMailTo() {
+	public String getMailToString() {
 		StringBuilder builder = new StringBuilder(MAILTO);
 		builder.append(licensePack.getUserIdentifier());
 		builder.append(MAILTO_SUBJECT);
@@ -84,6 +103,56 @@ public class LicenseMail {
 		builder.append(MAILTO_SEPARATOR);
 		builder.append(getDetails(MAILTO_SEPARATOR));
 		return builder.toString();
+	}
+
+	public File createEmlFile(File attachment) {
+		if (attachment != null && attachment.exists()) {
+			try {
+				return createEmlFile(licensePack.getRequestIdentifier(), licensePack.getUserIdentifier(), "From", //$NON-NLS-1$
+						LicensesCoreMessages.LicenseRequest_mailto_subject_lbl, getDetails(MAILTO_SEPARATOR),
+						attachment);
+			} catch (MessagingException | IOException e) {
+				Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, e.getMessage(), e);
+			}
+		} else {
+			Logger.getLogger(this.getClass().getName()).log(Level.SEVERE,
+					LicensesCoreMessages.LicenseRequest_mailto_error_attachment_not_exist);
+		}
+		return null;
+	}
+
+	private File createEmlFile(String emlFileName, String to, String from, String subject, String body,
+			File fileToAttache) throws MessagingException, IOException {
+
+		Message message = new MimeMessage(Session.getInstance(System.getProperties()));
+		message.setFrom(new InternetAddress(from));
+		message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+		message.setSubject(subject);
+		MimeBodyPart content = new MimeBodyPart();
+
+		content.setText(body);
+		Multipart multipart = new MimeMultipart();
+		multipart.addBodyPart(content);
+
+		MimeBodyPart attachment = new MimeBodyPart();
+		DataSource source = new FileDataSource(fileToAttache);
+		attachment.setDataHandler(new DataHandler(source));
+		attachment.setFileName(fileToAttache.getName());
+		multipart.addBodyPart(attachment);
+		message.setContent(multipart);
+
+		File emlFile = new File(System.getProperty("user.home") + File.separator + emlFileName + MAIL_EML_EXTENSION); //$NON-NLS-1$
+		if (!emlFile.exists() && emlFile.createNewFile()) {
+			try (FileOutputStream fileOutputStream = new FileOutputStream(emlFile)) {
+				message.writeTo(fileOutputStream);
+			}
+		} else {
+			String msg = NLS.bind(LicensesCoreMessages.LicenseRequest_mailto_error_file_already_exist,
+					emlFile.getAbsolutePath());
+			Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, msg);
+		}
+
+		return emlFile;
 	}
 
 }
