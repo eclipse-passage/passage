f1b149bf479d03df05741d56a7a54962108e4df5
elena.parovyshnaya <elena.parovyshnaya@gmail.com>
2019-10-22 20:38:23 +0300

    Bug 547289 - [Passage] Provide javadoc for org.eclipse.passage.lic.api package -- Appended javadoc for fields.
    Signed-off-by: Elena Parovyshnaia <elena.parovyshnaya@gmail.com>

--- bundles/org.eclipse.passage.lic.api/src/org/eclipse/passage/lic/api/conditions/LicensingCondition.java
+++ bundles/org.eclipse.passage.lic.api/src/org/eclipse/passage/lic/api/conditions/LicensingCondition.java
@@ -17,19 +17,33 @@ import java.util.Date;
 import org.eclipse.passage.lic.api.access.PermissionEmitter;
 
 /**
- * 
+ *
  * Defines the condition to be evaluated by {@link PermissionEmitter} <br/>
  * Obtained from {@link ConditionMiner}
- * 
+ *
  * @since 0.5.0
  *
  */
 public interface LicensingCondition {
-
+	/**
+	 * Returns unique identifier of a feature under licensing.
+	 *
+	 * @return feature identifier
+	 */
 	String getFeatureIdentifier();
 
+	/**
+     * Returns descriptor of the feature version allowed by this licensing condition.
+     *
+     * @return version descriptor
+     * */
 	String getMatchVersion();
 
+	/**
+     * Returns rule of version matching, like "perfect match" or "equal or greater".
+     *
+     * @return match rule
+     * */
 	String getMatchRule();
 
 	/**
@@ -49,12 +63,17 @@ public interface LicensingCondition {
 	Date getValidUntil();
 
 	/**
-	 * the type of condition like "time" or "hardware"
-	 * 
-	 * @return
+	 * The type of condition like "time" or "hardware".
+	 *
+	 * @return condition type
 	 */
 	String getConditionType();
 
+	/**
+     * Returns additional licensing requirements encoded in a single string value.
+     *
+     * @return description of additional demands of this licencing condition
+     * */
 	String getConditionExpression();
 
 }
