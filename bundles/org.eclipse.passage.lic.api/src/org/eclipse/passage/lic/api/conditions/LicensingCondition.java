e9b7343517ace2280e0a7dc6e678aae813a948a7
elena.parovyshnaya <elena.parovyshnaya@gmail.com>
2019-10-23 18:52:29 +0300

    Bug 547289 - [Passage] Provide javadoc for org.eclipse.passage.lic.api package -- formatted javadoc
    Signed-off-by: Elena Parovyshnaia <elena.parovyshnaya@gmail.com>

--- bundles/org.eclipse.passage.lic.api/src/org/eclipse/passage/lic/api/conditions/LicensingCondition.java
+++ bundles/org.eclipse.passage.lic.api/src/org/eclipse/passage/lic/api/conditions/LicensingCondition.java
@@ -17,66 +17,64 @@ import java.util.Date;
 import org.eclipse.passage.lic.api.access.PermissionEmitter;
 
 /**
- *
  * Defines the condition to be evaluated by {@link PermissionEmitter} <br/>
  * Obtained from {@link ConditionMiner}
  *
  * @since 0.5.0
- *
  */
 public interface LicensingCondition {
-	/**
-	 * Returns unique identifier of a feature under licensing.
-	 *
-	 * @return feature identifier
-	 */
+    /**
+     * Returns unique identifier of a feature under licensing.
+     *
+     * @return feature identifier
+     */
 	String getFeatureIdentifier();
 
-	/**
+    /**
      * Returns descriptor of the feature version allowed by this licensing condition.
      *
      * @return version descriptor
-     * */
+     */
 	String getMatchVersion();
 
-	/**
+    /**
      * Returns rule of version matching, like "perfect match" or "equal or greater".
      *
      * @return match rule
-     * */
+     */
 	String getMatchRule();
 
-	/**
-	 * Returns the validity period start date of this licensing condition. This is
-	 * the value of its <code>"validFrom"</code> attribute.
-	 *
-	 * @return the valid from
-	 */
+    /**
+     * Returns the validity period start date of this licensing condition. This is
+     * the value of its <code>"validFrom"</code> attribute.
+     *
+     * @return the valid from
+     */
 	Date getValidFrom();
 
-	/**
-	 * Returns the validity period end date of this licensing condition. This is the
-	 * value of its <code>"validUntil"</code> attribute.
-	 *
-	 * @return the valid until
-	 */
+    /**
+     * Returns the validity period end date of this licensing condition. This is the
+     * value of its <code>"validUntil"</code> attribute.
+     *
+     * @return the valid until
+     */
 	Date getValidUntil();
 
-	/**
-	 * The type of condition like "time" or "hardware".
-	 *
-	 * @return condition type
-	 */
+    /**
+     * The type of condition like "time" or "hardware".
+     *
+     * @return condition type
+     */
 	String getConditionType();
 
-	/**
+    /**
      * Returns additional data encoded in a single string value.
-	 * The expression is utilized by {@link PermissionEmitter} in conjunction with {@code conditionType}
+     * The expression is utilized by {@link PermissionEmitter} in conjunction with {@code conditionType}
      *
      * @return enlistment of additional information of this licencing condition
-	 * @see PermissionEmitter
-	 * @see #getConditionType
-     * */
+     * @see PermissionEmitter
+     * @see #getConditionType
+     */
 	String getConditionExpression();
 
 }
