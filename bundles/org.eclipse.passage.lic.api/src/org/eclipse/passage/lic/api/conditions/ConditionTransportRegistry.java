9930df6e661a9a81737d4ddf0be9ca8c77f67d6f
elena.parovyshnaya <elena.parovyshnaya@gmail.com>
2019-10-23 16:48:50 +0300

    Bug 547289 - [Passage] Provide javadoc for org.eclipse.passage.lic.api package -- added javadoc for ConditionTransportRegistry
    Signed-off-by: Elena Parovyshnaia <elena.parovyshnaya@gmail.com>

--- bundles/org.eclipse.passage.lic.api/src/org/eclipse/passage/lic/api/conditions/ConditionTransportRegistry.java
+++ bundles/org.eclipse.passage.lic.api/src/org/eclipse/passage/lic/api/conditions/ConditionTransportRegistry.java
@@ -14,14 +14,44 @@ package org.eclipse.passage.lic.api.conditions;
 
 import java.util.Map;
 
+/**
+ * Contract for a registry of {@link ConditionTransport}s.
+ *
+ * <p>As {@link LicensingCondition} can be persisted in any form, a {@link ConditionTransport}
+ * is associated with a particular  <i>content type</i> (like <i>application/json</i> or <i>application/xmk</i>).
+ * </p>
+ *
+ * @see ConditionTransport
+ * @see LicensingCondition
+ * */
 public interface ConditionTransportRegistry {
 
+	/**
+	 * Returns an aggregate of all registered {@link ConditionTransport}s.
+	 *
+	 * @return all registered {@link ConditionTransport}s.
+	 * */
 	Iterable<ConditionTransport> getConditionTransports();
 
+	/**
+	 * <p>
+	 * Returns {@link ConditionTransport}, which is registered to handle (read and write)
+	 * {@link LicensingCondition}s in the given {@code contentType}. The value is nullable.
+	 * </p>
+	 *
+	 * @return a {@link ConditionTransport} registered for the given {@code contentType}, if any, and {@code null} otherwise.
+	 * @see #registerConditionTransport(ConditionTransport, Map)
+	 * */
 	ConditionTransport getConditionTransportForContentType(String contentType);
 
+	/**
+	 * Appends the {@code transport} to the <i>registry</i> with the given set of properties (like content type).
+	 * */
 	void registerConditionTransport(ConditionTransport transport, Map<String, Object> properties);
 
+	/**
+	 * Removes the {@code transport} from the <i>registry</i>.
+	 * */
 	void unregisterConditionTransport(ConditionTransport transport, Map<String, Object> properties);
 
 }
