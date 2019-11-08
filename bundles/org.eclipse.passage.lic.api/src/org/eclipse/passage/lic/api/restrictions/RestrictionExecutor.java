58f420c468c42f269beac1a727eeeab777f7c4b3
elena.parovyshnaya <elena.parovyshnaya@gmail.com>
2019-11-08 12:30:33 +0300

    547295 Provide javadoc for org.eclipse.passage.lic.api.restrictions package
    Signed-off-by: Elena Parovyshnaia <elena.parovyshnaya@gmail.com>

--- bundles/org.eclipse.passage.lic.api/src/org/eclipse/passage/lic/api/restrictions/RestrictionExecutor.java
+++ bundles/org.eclipse.passage.lic.api/src/org/eclipse/passage/lic/api/restrictions/RestrictionExecutor.java
@@ -15,17 +15,32 @@ package org.eclipse.passage.lic.api.restrictions;
 import org.eclipse.passage.lic.api.LicensingResult;
 
 /**
- * 
- * Implement the {@link RestrictionVerdict}(s) for the licensed feature:
- * <li>early exit from command line tools with notice</li>
- * <li>blocking dialogs for UI application</li>
- * <li>filtering out the UI</li>
- * <li>blocking of bundles using OSGi level</li>
  *
+ * The service implementing the <i>restrictions execution</i> phase
+ * of <i> access cycle</i>.
+
  * @since 0.4.0
+ * @see org.eclipse.passage.lic.api
  */
 public interface RestrictionExecutor {
 
+	/**
+	 * <p>Applies the given {@link RestrictionVerdict}s for feature under licencing
+	 * to the current runtime of the program.</p>
+	 *
+	 * <p>Is capable to affect the program runtime environment the following ways:</p>
+	 * <ol>
+	 *    <li>early exit from command line tools with notice</li>
+	 *    <li>block dialogs for UI application</li>
+	 *    <li>filter out the UI</li>
+	 *    <li>block of bundles using OSGi level</li>
+	 * </ol>
+	 *
+	 * @param verdicts restriction instructions gained at the pagse of <i>permission examining</i> phase of <i>access cycle</i>
+	 * @return the final result summary of the current <i>access cycle</i>
+	 *
+	 * @since 0.4.0
+	 */
 	LicensingResult execute(Iterable<RestrictionVerdict> verdicts);
 
 }
