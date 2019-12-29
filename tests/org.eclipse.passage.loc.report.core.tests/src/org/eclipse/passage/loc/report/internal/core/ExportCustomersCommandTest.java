79bf5b64cbabf1ac93ade4380720f7ad86bdfd60
elena.parovyshnaya <elena.parovyshnaya@gmail.com>
2019-12-29 18:10:44 +0300

    Bug 558587 implement parameterized customer export
     - two more tests for failures
    
    Signed-off-by: elena.parovyshnaya <elena.parovyshnaya@gmail.com>

--- tests/org.eclipse.passage.loc.report.core.tests/src/org/eclipse/passage/loc/report/internal/core/ExportCustomersCommandTest.java
+++ tests/org.eclipse.passage.loc.report.core.tests/src/org/eclipse/passage/loc/report/internal/core/ExportCustomersCommandTest.java
@@ -24,62 +24,96 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import org.eclipse.passage.loc.yars.internal.api.ReportException;
-import org.junit.Before;
 import org.junit.Test;
 
-// FIXME test all sort of expected failures
-// FIXME integration-test over emf-based user registry for encoding purposes
-
+/**
+ * plain unit test
+ */
 @SuppressWarnings("restriction")
 public class ExportCustomersCommandTest {
 
-	private Path output;
+	@Test
+	public void tesCsvExport() {
+		Path output = outputFile(""); //$NON-NLS-1$
+		exportSilent(output);
+		assertOutputLooksAsExpected(output);
+	}
 
-	@Before
-	public void setUp() {
+	@Test(expected = ReportException.class)
+	public void testFailOnDirOutput() throws ReportException {
+		export(outputDir()); // failure is expected here
+	}
+
+	/**
+	 * Try to write to a file that cannot be created: most of practically used file
+	 * systems forbid file names longer than 255 symbols
+	 */
+	@Test(expected = ReportException.class)
+	public void testFailOnFileCannotBeCreated() throws ReportException {
+		String prefix = IntStream.range(0, 255)//
+				.mapToObj(i -> "x") //$NON-NLS-1$
+				.collect(Collectors.joining());
+		export(outputFile(prefix)); // failure is expected here
+	}
+
+	private Path outputFile(String prefix) {
 		try {
-			output = Files.createTempFile("test_", ".csv"); //$NON-NLS-1$ //$NON-NLS-2$
+			return Files.createTempFile("test_" + prefix, ".csv"); //$NON-NLS-1$ //$NON-NLS-2$
 		} catch (IOException e) {
 			assumeNoException("Failed to create temp file for test", e); //$NON-NLS-1$
+			return null;
 		}
 	}
 
-	@Test
-	public void tesCsvExport() {
+	private Path outputDir() {
 		try {
-			new ProductCustomersToCsv(new FakeCustomersBase())//
-					.export(fakeProducts(), output);
+			return Files.createTempDirectory("test_"); //$NON-NLS-1$
+		} catch (IOException e) {
+			assumeNoException("Failed to create temp directory for test", e); //$NON-NLS-1$
+			return null;
+		}
+	}
+
+	private void exportSilent(Path output) {
+		try {
+			export(output);
 		} catch (ReportException e) {
 			fail("Export failed"); //$NON-NLS-1$
 		}
-		assertOutputLooksAsExpected();
+	}
+
+	private void export(Path output) throws ReportException {
+		new ProductCustomersToCsv(new FakeCustomersBase())//
+				.export(fakeProducts(), output);
 	}
 
 	private Set<String> fakeProducts() {
 		return new HashSet<>(Arrays.asList("culture")); //$NON-NLS-1$
 	}
 
-	private void assertOutputLooksAsExpected() {
-		Set<String> result;
-		try {
-			result = new HashSet<>(//
-					Files.lines(output, StandardCharsets.UTF_8)//
-							.collect(Collectors.toSet()));
-		} catch (Throwable e) {
-			e.printStackTrace();
-			fail("Output is broken"); //$NON-NLS-1$
-			return;
-		}
-
+	private void assertOutputLooksAsExpected(Path output) {
 		Set<String> expectation = new HashSet<>(Arrays.asList(//
 				"email,name", //$NON-NLS-1$
 				"erwin.schrodinger@gmail.com,Erwin Rudolf Josef Alexander Schrödinger", //$NON-NLS-1$
 				"football-asia-cup-2007@gmail.com,오범석 呉範錫", //$NON-NLS-1$
 				"lomonosov_1711@yandex.com,Михайло Васильевич Ломоносов", //$NON-NLS-1$
 				"reiner.maria.rilke@gmail.com,René Karl Wilhelm Johann Josef Maria Rilke")); //$NON-NLS-1$
-		assertEquals(expectation, result);
+		assertEquals(expectation, results(output));
+	}
+
+	private Set<String> results(Path output) {
+		try {
+			return new HashSet<>(//
+					Files.lines(output, StandardCharsets.UTF_8)//
+							.collect(Collectors.toSet()));
+		} catch (Throwable e) {
+			e.printStackTrace();
+			fail("Output is broken"); //$NON-NLS-1$
+			return null;
+		}
 	}
 
 }
