9e70ea492e2127ef4a1d9ffe8f6d67f7eafb0cac
Nikifor Fedorov <zelenyhleb@gmail.com>
2020-02-17 12:07:06 +0300

    Bug 560181 - [Passage] add billing core basic implementation
    fixed tests variables and methods naming, removed redundant lists
    
    Signed-off-by: Nikifor Fedorov <zelenyhleb@gmail.com>

--- tests/org.eclipse.passage.loc.billing.core.tests/src/org/eclipse/passage/loc/billing/core/tests/UserLicensesTest.java
+++ tests/org.eclipse.passage.loc.billing.core.tests/src/org/eclipse/passage/loc/billing/core/tests/UserLicensesTest.java
@@ -17,6 +17,8 @@ import static org.junit.Assert.assertEquals;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.eclipse.passage.lic.users.UserLicenseDescriptor;
 import org.eclipse.passage.loc.internal.billing.core.UserLicenses;
@@ -25,7 +27,7 @@ import org.junit.Test;
 public class UserLicensesTest {
 
 	@Test
-	public void testGetAllLicenses() {
+	public void allLicenses() {
 		List<UserLicenseDescriptor> licenses1 = new ArrayList<UserLicenseDescriptor>();
 		licenses1.add(new FakeLicenseDescriptor("foo")); //$NON-NLS-1$
 		licenses1.add(new FakeLicenseDescriptor("bar")); //$NON-NLS-1$
@@ -34,20 +36,18 @@ public class UserLicensesTest {
 		licenses2.add(new FakeLicenseDescriptor("baz")); //$NON-NLS-1$
 		licenses2.add(new FakeLicenseDescriptor("foo")); //$NON-NLS-1$
 
-		UserLicenses userLicenses = new UserLicenses(
+		UserLicenses service = new UserLicenses(
 				Arrays.asList(new FakeUser[] { new FakeUser(licenses1), new FakeUser(licenses2) }));
 
-		List<UserLicenseDescriptor> expectedLicenses = new ArrayList<UserLicenseDescriptor>(licenses1);
-		expectedLicenses.addAll(licenses2);
-
-		assertEquals(expectedLicenses, userLicenses.getAllLicenses());
+		assertEquals(Stream.concat(licenses1.stream(), licenses2.stream()).collect(Collectors.toList()),
+				service.getAllLicenses());
 	}
 
 	@Test
-	public void testGetLicensesForProduct() {
-		String selectedProduct = "product"; //$NON-NLS-1$
-		FakeLicenseDescriptor license1 = new FakeLicenseDescriptor(selectedProduct);
-		FakeLicenseDescriptor license2 = new FakeLicenseDescriptor(selectedProduct);
+	public void licensesForProduct() {
+		String product = "product"; //$NON-NLS-1$
+		FakeLicenseDescriptor license1 = new FakeLicenseDescriptor(product);
+		FakeLicenseDescriptor license2 = new FakeLicenseDescriptor(product);
 
 		List<UserLicenseDescriptor> licenses1 = new ArrayList<UserLicenseDescriptor>();
 		licenses1.add(license1);
@@ -57,23 +57,19 @@ public class UserLicensesTest {
 		licenses2.add(new FakeLicenseDescriptor("baz")); //$NON-NLS-1$
 		licenses2.add(license2);
 
-		UserLicenses userLicenses = new UserLicenses(
+		UserLicenses service = new UserLicenses(
 				Arrays.asList(new FakeUser[] { new FakeUser(licenses1), new FakeUser(licenses2) }));
 
-		List<UserLicenseDescriptor> expectedLicenses = new ArrayList<UserLicenseDescriptor>();
-		expectedLicenses.add(license1);
-		expectedLicenses.add(license2);
-
-		assertEquals(expectedLicenses, userLicenses.getLicensesForProduct(selectedProduct));
+		assertEquals(Arrays.asList(license1, license2), service.getLicensesForProduct(product));
 	}
 
 	@Test
-	public void testGetLicensesForProductVersion() {
-		String selectedProduct = "product"; //$NON-NLS-1$
-		String selectedVersion = "version"; //$NON-NLS-1$
+	public void licensesForProductVersion() {
+		String product = "product"; //$NON-NLS-1$
+		String version = "version"; //$NON-NLS-1$
 
-		FakeLicenseDescriptor license1 = new FakeLicenseDescriptor(selectedProduct, selectedVersion);
-		FakeLicenseDescriptor license2 = new FakeLicenseDescriptor(selectedProduct); // default version is empty string
+		FakeLicenseDescriptor license1 = new FakeLicenseDescriptor(product, version);
+		FakeLicenseDescriptor license2 = new FakeLicenseDescriptor(product); // default version is empty string
 
 		List<UserLicenseDescriptor> licenses1 = new ArrayList<UserLicenseDescriptor>();
 		licenses1.add(license1);
@@ -83,13 +79,10 @@ public class UserLicensesTest {
 		licenses2.add(new FakeLicenseDescriptor("baz")); //$NON-NLS-1$
 		licenses2.add(license2);
 
-		UserLicenses userLicenses = new UserLicenses(
+		UserLicenses service = new UserLicenses(
 				Arrays.asList(new FakeUser[] { new FakeUser(licenses1), new FakeUser(licenses2) }));
 
-		List<UserLicenseDescriptor> expectedLicenses = new ArrayList<UserLicenseDescriptor>();
-		expectedLicenses.add(license1);
-
-		assertEquals(expectedLicenses, userLicenses.getLicensesForProductVersion(selectedProduct, selectedVersion));
+		assertEquals(Arrays.asList(license1), service.getLicensesForProductVersion(product, version));
 	}
 
 }
