996e13679592fedccf7aa4af9a15119e291d6e81
Alexander Fedorov <alexander.fedorov@arsysop.ru>
2020-02-23 15:46:49 +0300

    Bug 560433 - [Passage] add org.eclipse.passage.loc.workbench.tests
    Added org.eclipse.passage.loc.workbench.tests bundle
    Added ClassifierMetadataTest
    
    Signed-off-by: Alexander Fedorov <alexander.fedorov@arsysop.ru>

---
+++ bundles/org.eclipse.passage.loc.workbench/src/org/eclipse/passage/loc/internal/workbench/SelectRequest.java
@@ -0,0 +1,88 @@
+/*******************************************************************************
+ * Copyright (c) 2020 ArSysOp
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0/.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     ArSysOp - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.passage.loc.internal.workbench;
+
+import java.util.function.Supplier;
+
+import org.eclipse.passage.loc.jface.dialogs.Appearance;
+
+/**
+ * Encapsulates the information required to select the classifier
+ * 
+ * @since 0.6
+ *
+ * @param <R> target type to be selected
+ */
+public final class SelectRequest<R> {
+
+	private final Class<R> target;
+	private final String domain;
+	private final Supplier<Iterable<R>> input;
+	private final Appearance appearance;
+
+	/**
+	 * Creates a {@link SelectRequest} instance for target classifier that belongs
+	 * to a given domain, also specifies the input to select from and the dialog
+	 * appearance
+	 * 
+	 * @param target     the type of object to be selected, must not be
+	 *                   <code>null</code>
+	 * @param domain     the domain that target belongs to, must not be
+	 *                   <code>null</code>
+	 * @param input      the input to select from, must not be <code>null</code>
+	 * @param appearance the appearance of UI dialog, must not be <code>null</code>
+	 */
+	public SelectRequest(Class<R> target, String domain, Supplier<Iterable<R>> input, Appearance appearance) {
+		this.target = target;
+		this.domain = domain;
+		this.input = input;
+		this.appearance = appearance;
+	}
+
+	/**
+	 * The type of object to be selected
+	 * 
+	 * @return non-<code>null</code> target
+	 */
+	public Class<R> target() {
+		return target;
+	}
+
+	/**
+	 * The domain of object to be selected
+	 * 
+	 * @return non-<code>null</code> domain
+	 */
+	public String domain() {
+		return domain;
+	}
+
+	/**
+	 * The supplier of input to select from
+	 * 
+	 * @return non-<code>null</code> supplier of input
+	 */
+	public Supplier<Iterable<R>> input() {
+		return input;
+	}
+
+	/**
+	 * The appearance to use for UI
+	 * 
+	 * @return non-<code>null</code> apperance
+	 */
+	public Appearance appearance() {
+		return appearance;
+	}
+
+}
