cccafef6299d77bf777ac0e9704a293c2764549a
elena.parovyshnaya <elena.parovyshnaya@gmail.com>
2020-03-22 22:57:10 +0300

    Bug 561248 design client API entry point
     - add license headers
    
    Signed-off-by: elena.parovyshnaya <elena.parovyshnaya@gmail.com>

--- bundles/org.eclipse.passage.lic.e4.ui/src/org/eclipse/passage/lic/internal/e4/ui/addons/E4LicensingProcessor.java
+++ bundles/org.eclipse.passage.lic.e4.ui/src/org/eclipse/passage/lic/internal/e4/ui/addons/E4LicensingProcessor.java
@@ -1,6 +1,15 @@
-/*****************************************************************************************
- * Copyright (c) ArSysOp 2020
- *****************************************************************************************/
+/*******************************************************************************
+ * Copyright (c) 2020 ArSysOp
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0/.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     ArSysOp - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.passage.lic.internal.e4.ui.addons;
 
 import javax.inject.Inject;
