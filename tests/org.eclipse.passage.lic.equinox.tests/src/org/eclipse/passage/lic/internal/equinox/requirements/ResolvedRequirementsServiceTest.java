6db89cd786c05e42ed439a618b8f6a6f151812fb
elena.parovyshnaya <elena.parovyshnaya@gmail.com>
2020-04-05 21:35:32 +0300

    Bug 561435 API revision: requirements resolution: test coverage
      - remove duplicated license header
    
    
    Signed-off-by: elena.parovyshnaya <elena.parovyshnaya@gmail.com>

--- tests/org.eclipse.passage.lic.equinox.tests/src/org/eclipse/passage/lic/internal/equinox/requirements/ResolvedRequirementsServiceTest.java
+++ tests/org.eclipse.passage.lic.equinox.tests/src/org/eclipse/passage/lic/internal/equinox/requirements/ResolvedRequirementsServiceTest.java
@@ -12,18 +12,6 @@
  *******************************************************************************/
 package org.eclipse.passage.lic.internal.equinox.requirements;
 
-/*******************************************************************************
- * Copyright (c) 2020 ArSysOp
- *
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License 2.0 which is available at
- * https://www.eclipse.org/legal/epl-2.0/.
- *
- * SPDX-License-Identifier: EPL-2.0
- *
- * Contributors:
- *     ArSysOp - initial API and implementation
- *******************************************************************************/
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assume.assumeTrue;
