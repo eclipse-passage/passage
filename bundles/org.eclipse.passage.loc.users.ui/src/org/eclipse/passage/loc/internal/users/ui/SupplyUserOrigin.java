cacc8c544a8e90552154e5bf637e5e0c059ee486
Alexander Fedorov <alexander.fedorov@arsysop.ru>
2020-04-05 12:08:06 +0300

    Bug 548115 - allow to create product version from issue license wizard
    Register ClassSupply implementations for "users" domain.
    
    Signed-off-by: Alexander Fedorov <alexander.fedorov@arsysop.ru>

---
+++ bundles/org.eclipse.passage.loc.users.ui/src/org/eclipse/passage/loc/internal/users/ui/SupplyUserOrigin.java
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ * Copyright (c) 2020 ArSysOp
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0/.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     ArSysOp - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.passage.loc.internal.users.ui;
+
+import java.util.Optional;
+
+import org.eclipse.passage.lic.internal.api.MandatoryService;
+import org.eclipse.passage.lic.users.UserOriginDescriptor;
+import org.eclipse.passage.loc.internal.api.InstanceSupply;
+import org.eclipse.passage.loc.internal.workbench.SelectRoot;
+
+public class SupplyUserOrigin implements InstanceSupply<UserOriginDescriptor> {
+
+	private final MandatoryService context;
+
+	public SupplyUserOrigin(MandatoryService context) {
+		this.context = context;
+	}
+
+	@Override
+	public Optional<UserOriginDescriptor> supply() {
+		return new SelectRoot<UserOriginDescriptor>(new SelectUserOrigin(context).get(), context).get();
+	}
+
+}
