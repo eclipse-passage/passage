1029aca5ee1e90f634df3b7c420c75794a5378d2
Alexander Fedorov <alexander.fedorov@arsysop.ru>
2020-05-03 10:39:11 +0300

    Bug 562012 - license is not recognized when USB disk is plugged
    Read disks dynamically in another thread.
    Catch potential emitter failure in main cycle.
    
    Signed-off-by: Alexander Fedorov <alexander.fedorov@arsysop.ru>

--- bundles/org.eclipse.passage.lic.oshi/src/org/eclipse/passage/lic/internal/oshi/OshiPermissionEmitter.java
+++ bundles/org.eclipse.passage.lic.oshi/src/org/eclipse/passage/lic/internal/oshi/OshiPermissionEmitter.java
@@ -16,15 +16,21 @@ import static org.eclipse.passage.lic.base.LicensingProperties.LICENSING_CONDITI
 import static org.eclipse.passage.lic.base.LicensingProperties.LICENSING_CONDITION_TYPE_ID;
 import static org.eclipse.passage.lic.base.LicensingProperties.LICENSING_CONDITION_TYPE_NAME;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
+import org.eclipse.passage.lic.api.LicensingReporter;
 import org.eclipse.passage.lic.api.access.PermissionEmitter;
 import org.eclipse.passage.lic.api.inspector.HardwareInspector;
+import org.eclipse.passage.lic.base.LicensingResults;
+import org.eclipse.passage.lic.base.SystemReporter;
 import org.eclipse.passage.lic.base.access.BasePermissionEmitter;
 import org.eclipse.passage.lic.base.conditions.LicensingConditions;
+import org.eclipse.passage.lic.internal.oshi.i18n.OshiMessages;
 import org.eclipse.passage.lic.oshi.OshiHal;
-import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
@@ -37,19 +43,17 @@ import oshi.hardware.HWDiskStore;
 public class OshiPermissionEmitter extends BasePermissionEmitter implements PermissionEmitter {
 
 	private HardwareInspector hardwareInspector;
+	private LicensingReporter licensingReporter = new SystemReporter();
 
-	private final List<String> serials = new ArrayList<>();
-
-	public OshiPermissionEmitter() {
+	@Reference
+	public void bindLicensingReporter(LicensingReporter reporter) {
+		this.licensingReporter = reporter;
 	}
 
-	@Activate
-	public void activate() {
-		HWDiskStore[] diskStores = new SystemInfo().getHardware().getDiskStores();
-		for (HWDiskStore hwDiskStore : diskStores) {
-			serials.add(hwDiskStore.getSerial());
+	public void unbindLicensingReporter(LicensingReporter reporter) {
+		if (this.licensingReporter == reporter) {
+			this.licensingReporter = new SystemReporter();
 		}
-
 	}
 
 	@Reference
@@ -68,11 +72,23 @@ public class OshiPermissionEmitter extends BasePermissionEmitter implements Perm
 		// FIXME: EP: fast hack for 562012,
 		// to be solved properly with formats alternations in 0.9
 		if (key.equals(HardwareInspector.PROPERTY_HWDISK_SERIAL)) {
-			return serials.stream() //
-					.anyMatch(disk -> LicensingConditions.evaluateSegmentValue(expected, disk));
+			return disks().stream() //
+					.anyMatch(disk -> LicensingConditions.evaluateSegmentValue(expected, disk.getSerial()));
 		}
 		String actual = hardwareInspector.inspectProperty(key);
 		return LicensingConditions.evaluateSegmentValue(expected, actual);
+	}
+
+	private List<HWDiskStore> disks() {
+		try {
+			return Arrays.asList(CompletableFuture.supplyAsync(//
+					() -> new SystemInfo().getHardware().getDiskStores()).get());
+		} catch (InterruptedException | ExecutionException e) {
+			licensingReporter.logResult(//
+					LicensingResults.createError(//
+							OshiMessages.OshiPermissionEmitter_e_retrieve_disks, getClass().getName(), e));
+			return Collections.emptyList();
+		}
 	}
 
 }
