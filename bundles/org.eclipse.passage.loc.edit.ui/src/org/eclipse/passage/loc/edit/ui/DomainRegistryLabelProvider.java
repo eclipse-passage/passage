5353f2dbdba360daa70d0143cefb96812cfdc7a5
Alexander Fedorov <alexander.fedorov@arsysop.ru>
2020-06-01 18:40:20 +0300

    Bug 563786 - [Passage] rework operator toolbar
    Rework DomainRegistryLabelProvider
    
    Signed-off-by: Alexander Fedorov <alexander.fedorov@arsysop.ru>

--- bundles/org.eclipse.passage.loc.edit.ui/src/org/eclipse/passage/loc/edit/ui/DomainRegistryLabelProvider.java
+++ bundles/org.eclipse.passage.loc.edit.ui/src/org/eclipse/passage/loc/edit/ui/DomainRegistryLabelProvider.java
@@ -15,35 +15,17 @@ package org.eclipse.passage.loc.edit.ui;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.jface.viewers.LabelProvider;
-import org.eclipse.passage.lic.emf.ecore.EditingDomainRegistry;
 import org.eclipse.passage.lic.features.model.meta.FeaturesPackage;
-import org.eclipse.passage.lic.features.registry.FeatureRegistry;
 import org.eclipse.passage.lic.jface.resource.LicensingImages;
 import org.eclipse.passage.lic.licenses.model.meta.LicensesPackage;
-import org.eclipse.passage.lic.licenses.registry.LicenseRegistry;
 import org.eclipse.passage.lic.products.model.meta.ProductsPackage;
-import org.eclipse.passage.lic.products.registry.ProductRegistry;
 import org.eclipse.passage.lic.users.model.meta.UsersPackage;
-import org.eclipse.passage.lic.users.registry.UserRegistry;
-import org.eclipse.passage.loc.internal.edit.ui.i18n.EditUiMessages;
 import org.eclipse.swt.graphics.Image;
 
 class DomainRegistryLabelProvider extends LabelProvider {
 
 	@Override
 	public String getText(Object element) {
-		if (element instanceof FeatureRegistry) {
-			return EditUiMessages.DomainRegistryLabelProvider_features;
-		}
-		if (element instanceof ProductRegistry) {
-			return EditUiMessages.DomainRegistryLabelProvider_products;
-		}
-		if (element instanceof UserRegistry) {
-			return EditUiMessages.DomainRegistryLabelProvider_users;
-		}
-		if (element instanceof LicenseRegistry) {
-			return EditUiMessages.DomainRegistryLabelProvider_licenses;
-		}
 		if (element instanceof Resource) {
 			Resource resource = (Resource) element;
 			URI uri = resource.getURI();
@@ -54,10 +36,6 @@ class DomainRegistryLabelProvider extends LabelProvider {
 
 	@Override
 	public Image getImage(Object element) {
-		if (element instanceof EditingDomainRegistry<?>) {
-			EditingDomainRegistry<?> registry = (EditingDomainRegistry<?>) element;
-			return LicensingImages.getImage(registry.getContentClassifier().getName());
-		}
 		if (element instanceof Resource) {
 			Resource resource = (Resource) element;
 			URI uri = resource.getURI();
--- bundles/org.eclipse.passage.loc.edit.ui/src/org/eclipse/passage/loc/internal/edit/ui/i18n/EditUiMessages.java
+++ bundles/org.eclipse.passage.loc.edit.ui/src/org/eclipse/passage/loc/internal/edit/ui/i18n/EditUiMessages.java
@@ -16,10 +16,6 @@ import org.eclipse.osgi.util.NLS;
 
 public class EditUiMessages extends NLS {
 	private static final String BUNDLE_NAME = "org.eclipse.passage.loc.internal.edit.ui.i18n.EditUiMessages"; //$NON-NLS-1$
-	public static String DomainRegistryLabelProvider_features;
-	public static String DomainRegistryLabelProvider_licenses;
-	public static String DomainRegistryLabelProvider_products;
-	public static String DomainRegistryLabelProvider_users;
 	public static String DomainRegistryRemoveHandler_mesage;
 	public static String DomainRegistryRemoveHandler_title;
 	static {
