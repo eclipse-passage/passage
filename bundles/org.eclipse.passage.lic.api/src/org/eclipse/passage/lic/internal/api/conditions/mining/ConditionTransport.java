e6b65590953bcd675e685d0f5a732b8ea2cc22e5
elena.parovyshnaya <elena.parovyshnaya@gmail.com>
2020-06-21 08:51:06 +0300

    Bug 564420 - move HC condition miner to new interfaces
     - port `ConditionTransport` to new `Condition` interface
    
    Signed-off-by: elena.parovyshnaya <elena.parovyshnaya@gmail.com>

---
+++ bundles/org.eclipse.passage.lic.api/src/org/eclipse/passage/lic/internal/api/conditions/mining/ConditionTransport.java
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ * Copyright (c) 2020 ArSysOp
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0/.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     ArSysOp - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.passage.lic.internal.api.conditions.mining;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Collection;
+
+import org.eclipse.passage.lic.internal.api.conditions.Condition;
+
+/**
+ * Persistence interface for {@link Condition}(s).
+ */
+public interface ConditionTransport {
+
+	/**
+	 * Reads {@link Condition}(s) from the given {@link InputStream}. Stream remains
+	 * open.
+	 */
+	Collection<Condition> read(InputStream input) throws IOException;
+
+	/**
+	 * Writes {@link Condition}(s) to the given {@link OutputStream}. Stream remains
+	 * open.
+	 */
+	void write(Collection<Condition> conditions, OutputStream output) throws IOException;
+
+}
