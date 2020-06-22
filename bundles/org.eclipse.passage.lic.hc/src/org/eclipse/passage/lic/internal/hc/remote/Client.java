298d451aa464fc8a9d91adf14f08ca549dcdbfbf
elena.parovyshnaya <elena.parovyshnaya@gmail.com>
2020-06-22 19:41:55 +0300

    Bug 564420 move HC condition miner to new interfaces
     - interfaces for the new approach
    
    Signed-off-by: elena.parovyshnaya <elena.parovyshnaya@gmail.com>

---
+++ bundles/org.eclipse.passage.lic.hc/src/org/eclipse/passage/lic/internal/hc/remote/Client.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 2020 ArSysOp
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0/.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     ArSysOp - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.passage.lic.internal.hc.remote;
+
+import java.util.Collection;
+
+import org.eclipse.passage.lic.internal.api.conditions.Condition;
+import org.eclipse.passage.lic.internal.api.conditions.mining.ConditionMiningException;
+
+@SuppressWarnings("restriction")
+public interface Client<C> {
+
+	Collection<Condition> remoteConditions(Request<C> request, ResponseHandler miner) throws ConditionMiningException;
+
+}
