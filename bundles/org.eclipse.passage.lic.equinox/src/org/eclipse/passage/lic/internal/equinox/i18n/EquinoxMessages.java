b1af938c030671cbcbb7471fb813fa6c321488b6
Alexander Fedorov <alexander.fedorov@arsysop.ru>
2020-08-02 12:38:25 +0300

    Bug 547540 - [Passage] Remove EquinoxRestrictions#getRestrictionStatus
    Removed method and related resources
    
    Signed-off-by: Alexander Fedorov <alexander.fedorov@arsysop.ru>

--- bundles/org.eclipse.passage.lic.equinox/src/org/eclipse/passage/lic/internal/equinox/i18n/EquinoxMessages.java
+++ bundles/org.eclipse.passage.lic.equinox/src/org/eclipse/passage/lic/internal/equinox/i18n/EquinoxMessages.java
@@ -23,9 +23,6 @@ public class EquinoxMessages extends NLS {
 	public static String EquinoxPassage_no_framework;
 
 	public static String EquinoxPaths_uri_retrieval_error;
-	public static String EquinoxRestrictions_feature_is_licensed;
-	public static String EquinoxRestrictions_feature_is_not_licensed;
-	public static String EquinoxRestrictions_title;
 	public static String ComponentConfigurationResolver_error_invalid_bundle_context;
 	public static String ComponentConfigurationResolver_error_invalid_component_rt;
 	public static String ComponentRequirements_error_no_resource;
