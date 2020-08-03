69bb72b5d0a54e1ca12ccce9c57edd3eaa9c88a8
elena.parovyshnaya <elena.parovyshnaya@gmail.com>
2020-08-03 16:03:25 +0300

    Bug 565684 API revision | diagnostic
     let `permissions` AccessCycle facility ignore `ConditionPack`
    permissions factorization as far as it's not demanded
    
    Signed-off-by: elena.parovyshnaya <elena.parovyshnaya@gmail.com>

--- bundles/org.eclipse.passage.lic.base/src/org/eclipse/passage/lic/internal/base/access/Permissions.java
+++ bundles/org.eclipse.passage.lic.base/src/org/eclipse/passage/lic/internal/base/access/Permissions.java
@@ -12,9 +12,12 @@
  *******************************************************************************/
 package org.eclipse.passage.lic.internal.base.access;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Optional;
+import java.util.function.BiFunction;
 import java.util.function.Supplier;
-import java.util.stream.Collectors;
 
 import org.eclipse.passage.lic.internal.api.LicensedProduct;
 import org.eclipse.passage.lic.internal.api.ServiceInvocationResult;
@@ -24,6 +27,8 @@ import org.eclipse.passage.lic.internal.api.conditions.evaluation.Permission;
 import org.eclipse.passage.lic.internal.api.conditions.evaluation.PermissionEmittingService;
 import org.eclipse.passage.lic.internal.api.registry.Registry;
 import org.eclipse.passage.lic.internal.api.registry.StringServiceId;
+import org.eclipse.passage.lic.internal.base.BaseServiceInvocationResult;
+import org.eclipse.passage.lic.internal.base.SumOfCollections;
 
 /**
  * FIXME: Has public visibility only for testing.
@@ -46,11 +51,37 @@ public final class Permissions implements Supplier<ServiceInvocationResult<Colle
 	public ServiceInvocationResult<Collection<Permission>> get() {
 		return registry.services().stream() //
 				.map(service -> service.emit(conditions, product))//
-				.flatMap(Collection::stream) //
-				.filter(Emission::successful) //
-				.map(Emission::permissions) //
-				.flatMap(Collection::stream) //
-				.collect(Collectors.toSet());
+				.reduce(new BaseServiceInvocationResult.Sum<>(new SumOfCollections<Emission>()))//
+				.map(r -> new BaseServiceInvocationResult<Collection<Permission>>(//
+						r.diagnostic(), //
+						allPermissions(r.data())))//
+				.orElseGet(BaseServiceInvocationResult<Collection<Permission>>::new);
+	}
+
+	private Collection<Permission> allPermissions(Optional<Collection<Emission>> emissions) {
+		if (!emissions.isPresent()) {
+			return Collections.emptyList();
+		}
+		return emissions.get().stream() //
+				.reduce(//
+						new ArrayList<Permission>(), //
+						new SumEmissionsToData(), //
+						new SumOfCollections<Permission>());
+	}
+
+	/**
+	 * Here we do not need `ConditionPack` granularity, thus unfold each one to
+	 * amass all the permissions we have.
+	 */
+	private final static class SumEmissionsToData
+			implements BiFunction<Collection<Permission>, Emission, Collection<Permission>> {
+
+		@Override
+		public Collection<Permission> apply(Collection<Permission> sum, Emission emission) {
+			sum.addAll(emission.permissions());
+			return sum;
+		}
+
 	}
 
 }
