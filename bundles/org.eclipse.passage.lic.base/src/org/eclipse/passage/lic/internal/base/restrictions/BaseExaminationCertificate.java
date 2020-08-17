27ad7640ccc793f3cfc35c9fbd65074bbb72ce82
eparovyshnaya <elena.parovyshnaya@gmail.com>
2020-08-17 18:42:08 +0300

    Bug 565987 - API revision | implement license importing flow
     Construct ExaminationCertificate safely
    
    Signed-off-by: eparovyshnaya <elena.parovyshnaya@gmail.com>

--- bundles/org.eclipse.passage.lic.base/src/org/eclipse/passage/lic/internal/base/restrictions/BaseExaminationCertificate.java
+++ bundles/org.eclipse.passage.lic.base/src/org/eclipse/passage/lic/internal/base/restrictions/BaseExaminationCertificate.java
@@ -14,6 +14,7 @@ package org.eclipse.passage.lic.internal.base.restrictions;
 
 import java.time.ZonedDateTime;
 import java.util.Collection;
+import java.util.Objects;
 
 import org.eclipse.passage.lic.internal.api.conditions.evaluation.Permission;
 import org.eclipse.passage.lic.internal.api.restrictions.ExaminationCertificate;
@@ -27,6 +28,8 @@ public final class BaseExaminationCertificate implements ExaminationCertificate 
 	private final ZonedDateTime stamp;
 
 	public BaseExaminationCertificate(Collection<Permission> participants, Collection<Restriction> restrictions) {
+		Objects.requireNonNull(participants, "BaseExaminationCertificate::participants"); //$NON-NLS-1$
+		Objects.requireNonNull(restrictions, "BaseExaminationCertificate::restrictions"); //$NON-NLS-1$
 		this.participants = participants;
 		this.restrictions = restrictions;
 		this.stamp = ZonedDateTime.now();
