b76d379ffaccf22acd56e5180311bb6c85e64a9d
eparovyshnaya <elena.parovyshnaya@gmail.com>
2020-08-25 17:04:58 +0300

    Bug 566329 provide a way to summarize total product licensing status
    Reimplement inspection handlers for
     - license coverage and
      - runtime environments
    basing on new workbench facilities
    
    
    Signed-off-by: eparovyshnaya <elena.parovyshnaya@gmail.com>

---
+++ bundles/org.eclipse.passage.lic.e4.ui/src/org/eclipse/passage/lic/internal/e4/ui/handlers/InspectEnvironmentHandler.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2018, 2020 ArSysOp
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0/.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     ArSysOp - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.passage.lic.internal.e4.ui.handlers;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.passage.lic.internal.jface.dialogs.licensing.EnvironmentStateDialog;
+import org.eclipse.swt.widgets.Shell;
+
+@SuppressWarnings("restriction")
+public final class InspectEnvironmentHandler {
+
+	@Execute
+	public void execute(Shell shell) {
+		new EnvironmentStateDialog(shell).open();
+	}
+
+}
