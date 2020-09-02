1525edab23d18448ee0b0f214447b3f4660abfa7
Alexander Fedorov <alexander.fedorov@arsysop.ru>
2020-09-02 21:41:41 +0300

    Bug 566287 API revision | remove deprecated code
    Move LIC Base to LOC internal
    Remove LicensingVersions
    
    Signed-off-by: Alexander Fedorov <alexander.fedorov@arsysop.ru>

--- bundles/org.eclipse.passage.loc.api/src/org/eclipse/passage/loc/internal/api/BaseLicensingResult.java
+++ bundles/org.eclipse.passage.loc.api/src/org/eclipse/passage/loc/internal/api/BaseLicensingResult.java
@@ -10,7 +10,7 @@
  * Contributors:
  *     ArSysOp - initial API and implementation
  *******************************************************************************/
-package org.eclipse.passage.lic.base;
+package org.eclipse.passage.loc.internal.api;
 
 import java.util.ArrayList;
 import java.util.Collections;
