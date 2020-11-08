a3d644f92cb6d12be392f4904c029ffb042ccd63
eparovyshnaya <elena.parovyshnaya@gmail.com>
2020-11-08 12:54:18 +0300

    Bug 567035 implement floating license pack issuing
     File extensions for floating license pack files.
    
    Signed-off-by: eparovyshnaya <elena.parovyshnaya@gmail.com>

---
+++ bundles/org.eclipse.passage.lic.floating/src/org/eclipse/passage/lic/floating/FloatingFileExtensions.java
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ * Copyright (c) 2020 ArSysOp
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0/.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     ArSysOp - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.passage.lic.floating;
+
+import org.eclipse.passage.lic.internal.base.io.PassageFileExtension;
+
+public abstract class FloatingFileExtensions extends PassageFileExtension {
+
+	public static final class LicenseEncrypted extends FloatingFileExtensions {
+
+		@Override
+		public String get() {
+			return ".flicen"; //$NON-NLS-1$
+		}
+
+	}
+
+	public static final class LicenseDecrypted extends FloatingFileExtensions {
+
+		@Override
+		public String get() {
+			return ".flic"; //$NON-NLS-1$
+		}
+
+	}
+
+	public static final class LicenseAccessDecrypted extends FloatingFileExtensions {
+
+		@Override
+		public String get() {
+			return ".fla"; //$NON-NLS-1$
+		}
+
+	}
+
+	public static final class LicenseAccessEncrypted extends FloatingFileExtensions {
+
+		@Override
+		public String get() {
+			return ".flaen"; //$NON-NLS-1$
+		}
+
+	}
+}
