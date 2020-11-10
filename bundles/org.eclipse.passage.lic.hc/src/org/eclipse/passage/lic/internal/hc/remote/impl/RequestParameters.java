7eba35706e1f62eda63b993ca310285c75753bb4
eparovyshnaya <elena.parovyshnaya@gmail.com>
2020-11-10 15:20:27 +0300

    Bug 568632 Implement feature grant acquire/release
     Invite Acquiring/releasing service interfaces
    
    Signed-off-by: eparovyshnaya <elena.parovyshnaya@gmail.com>

--- bundles/org.eclipse.passage.lic.hc/src/org/eclipse/passage/lic/internal/hc/remote/impl/RequestParameters.java
+++ bundles/org.eclipse.passage.lic.hc/src/org/eclipse/passage/lic/internal/hc/remote/impl/RequestParameters.java
@@ -15,26 +15,63 @@ package org.eclipse.passage.lic.internal.hc.remote.impl;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import org.eclipse.passage.lic.floating.model.api.FloatingLicenseAccess;
+import org.eclipse.passage.lic.internal.api.LicensedProduct;
 import org.eclipse.passage.lic.internal.api.LicensingException;
+import org.eclipse.passage.lic.internal.api.conditions.ConditionAction;
+import org.eclipse.passage.lic.internal.api.conditions.mining.ContentType;
 import org.eclipse.passage.lic.internal.base.NamedData;
+import org.eclipse.passage.lic.internal.base.ProductIdentifier;
+import org.eclipse.passage.lic.internal.base.ProductVersion;
+import org.eclipse.passage.lic.internal.base.conditions.mining.LicensingContentType;
 import org.eclipse.passage.lic.internal.hc.i18n.AccessMessages;
+import org.eclipse.passage.lic.internal.net.LicenseUser;
+import org.eclipse.passage.lic.internal.net.LicensingAction;
+import org.eclipse.passage.lic.internal.net.ServerAuthenticationExpression;
+import org.eclipse.passage.lic.internal.net.ServerAuthenticationType;
 
 public abstract class RequestParameters {
 
-	protected RequestParameters() {
+	private final LicensedProduct product;
+	private final FloatingLicenseAccess access;
+
+	protected RequestParameters(LicensedProduct product, FloatingLicenseAccess access) {
+		this.product = product;
+		this.access = access;
 	}
 
 	public final String query() throws LicensingException {
 		StringBuilder params = new StringBuilder();
-		Arrays.stream(parameters())//
+		parameters().stream()//
 				.map(NamedData.Writable<String>::new)//
 				.forEach(writable -> writable.write(params, "=", "&")); //$NON-NLS-1$ //$NON-NLS-2$
 		return '?' + params.toString();
 	}
 
 	@SuppressWarnings("rawtypes")
-	protected abstract NamedData[] parameters() throws LicensingException;
+	private List<NamedData> parameters() throws LicensingException {
+		return Arrays.asList(//
+				generalParameters(), //
+				actionParameters()).stream() //
+				.flatMap(List::stream)//
+				.collect(Collectors.toList());
+	}
+
+	@SuppressWarnings("rawtypes")
+	private List<NamedData> generalParameters() throws LicensingException {
+		return Arrays.asList( //
+				new ProductIdentifier(encode(product.identifier())), //
+				new ProductVersion(encode(product.version())), //
+				new LicensingAction(action()), //
+				new LicensingContentType(new ContentType.Xml()), //
+				new LicenseUser(access.getUser()), //
+				new ServerAuthenticationType(access.getServer()), //
+				new ServerAuthenticationExpression(access.getServer()));
+
+	}
 
 	protected String encode(String value) throws LicensingException {
 		try {
@@ -43,5 +80,10 @@ public abstract class RequestParameters {
 			throw new LicensingException(AccessMessages.RequestParameters_encoding_failed, e);
 		}
 	}
+
+	protected abstract ConditionAction action() throws LicensingException;
+
+	@SuppressWarnings("rawtypes")
+	protected abstract List<NamedData> actionParameters() throws LicensingException;
 
 }
--- bundles/org.eclipse.passage.lic.hc/src/org/eclipse/passage/lic/internal/hc/remote/impl/acquire/AcquireRequestParameters.java
+++ bundles/org.eclipse.passage.lic.hc/src/org/eclipse/passage/lic/internal/hc/remote/impl/acquire/AcquireRequestParameters.java
@@ -12,46 +12,34 @@
  *******************************************************************************/
 package org.eclipse.passage.lic.internal.hc.remote.impl.acquire;
 
+import java.util.Collections;
+import java.util.List;
+
 import org.eclipse.passage.lic.floating.model.api.FloatingLicenseAccess;
 import org.eclipse.passage.lic.internal.api.LicensedProduct;
 import org.eclipse.passage.lic.internal.api.LicensingException;
 import org.eclipse.passage.lic.internal.api.conditions.ConditionAction;
-import org.eclipse.passage.lic.internal.api.conditions.mining.ContentType;
-import org.eclipse.passage.lic.internal.base.FeatureIdentifier;
 import org.eclipse.passage.lic.internal.base.NamedData;
 import org.eclipse.passage.lic.internal.base.ProductIdentifier;
-import org.eclipse.passage.lic.internal.base.ProductVersion;
-import org.eclipse.passage.lic.internal.base.conditions.mining.LicensingContentType;
 import org.eclipse.passage.lic.internal.hc.remote.impl.RequestParameters;
-import org.eclipse.passage.lic.internal.net.LicenseUser;
-import org.eclipse.passage.lic.internal.net.LicensingAction;
-import org.eclipse.passage.lic.internal.net.ServerAuthenticationExpression;
-import org.eclipse.passage.lic.internal.net.ServerAuthenticationType;
 
 final class AcquireRequestParameters extends RequestParameters {
 
-	private final LicensedProduct product;
 	private final String feature;
-	private final FloatingLicenseAccess access;
 
 	AcquireRequestParameters(LicensedProduct product, String feature, FloatingLicenseAccess access) {
-		this.product = product;
+		super(product, access);
 		this.feature = feature;
-		this.access = access;
 	}
 
 	@Override
-	@SuppressWarnings("rawtypes")
-	protected NamedData[] parameters() throws LicensingException {
-		return new NamedData[] { //
-				new ProductIdentifier(encode(product.identifier())), //
-				new ProductVersion(encode(product.version())), //
-				new FeatureIdentifier(encode(feature)), //
-				new LicensingAction(new ConditionAction.Acquire()), //
-				new LicensingContentType(new ContentType.Xml()), //
-				new LicenseUser(access.getUser()), //
-				new ServerAuthenticationType(access.getServer()), //
-				new ServerAuthenticationExpression(access.getServer()) };
+	protected ConditionAction action() {
+		return new ConditionAction.Acquire();
 	}
 
+	@SuppressWarnings("rawtypes")
+	@Override
+	protected List<NamedData> actionParameters() throws LicensingException {
+		return Collections.singletonList(new ProductIdentifier(encode(feature)));
+	}
 }
