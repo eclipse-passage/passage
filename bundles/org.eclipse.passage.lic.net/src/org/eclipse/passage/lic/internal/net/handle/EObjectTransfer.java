9563c1ade7034a023891fc36c024e21da215a5da
eparovyshnaya <elena.parovyshnaya@gmail.com>
2021-02-15 17:32:47 +0300

    Bug 571147  'lic.net' with common facilities used by Passage Servers
    Move general-purpose NetResponses to 'net' for reuse
    
    Signed-off-by: eparovyshnaya <elena.parovyshnaya@gmail.com>

--- bundles/org.eclipse.passage.lic.net/src/org/eclipse/passage/lic/internal/net/handle/EObjectTransfer.java
+++ bundles/org.eclipse.passage.lic.net/src/org/eclipse/passage/lic/internal/net/handle/EObjectTransfer.java
@@ -10,7 +10,7 @@
  * Contributors:
  *     ArSysOp - initial API and implementation
  *******************************************************************************/
-package org.eclipse.passage.lbc.internal.base;
+package org.eclipse.passage.lic.internal.net.handle;
 
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.passage.lic.internal.api.LicensingException;
