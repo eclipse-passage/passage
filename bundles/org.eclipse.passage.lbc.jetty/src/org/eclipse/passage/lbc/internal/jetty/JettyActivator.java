8c924b4c26ef385614a5784b1684f35729e71dcc
Elena Parovyshnaia <elena.parovyshnaya@gmail.com>
2021-03-21 15:35:27 +0300

    Bug 572145 - [Passage][Floating] extend logging
    plug logging programmatically
    
    Signed-off-by: eparovyshnaya <elena.parovyshnaya@gmail.com>

--- bundles/org.eclipse.passage.lbc.jetty/src/org/eclipse/passage/lbc/internal/jetty/JettyActivator.java
+++ bundles/org.eclipse.passage.lbc.jetty/src/org/eclipse/passage/lbc/internal/jetty/JettyActivator.java
@@ -12,23 +12,30 @@
  *******************************************************************************/
 package org.eclipse.passage.lbc.internal.jetty;
 
+import java.nio.file.Path;
+
+import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.Platform;
-import org.eclipse.passage.lbc.internal.base.FlotingRequestHandled;
 import org.eclipse.passage.lbc.internal.base.EagerFloatingState;
+import org.eclipse.passage.lbc.internal.base.FlotingRequestHandled;
 import org.eclipse.passage.lbc.internal.base.api.FloatingState;
+import org.eclipse.passage.lic.internal.base.logging.Logging;
 import org.eclipse.passage.lic.internal.jetty.JettyHandler;
 import org.eclipse.passage.lic.internal.jetty.JettyServer;
 import org.eclipse.passage.lic.internal.net.connect.Port;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
 
 public class JettyActivator implements BundleActivator {
 
 	private final JettyServer jetty;
-	private final FloatingState state = new EagerFloatingState();
+	private final FloatingState state;
 
 	public JettyActivator() {
-		jetty = new JettyServer(this::handler);
+		configureLogging();
+		this.jetty = new JettyServer(this::handler);
+		this.state = new EagerFloatingState();
 	}
 
 	@Override
@@ -43,6 +50,16 @@ public class JettyActivator implements BundleActivator {
 
 	private JettyHandler handler() {
 		return new JettyHandler(request -> new FlotingRequestHandled(new StatedRequest(request, state)).get());
+	}
+
+	private void configureLogging() {
+		new Logging(this::logConfig).configure();
+	}
+
+	private Path logConfig() throws Exception {
+		return FileLocator.getBundleFile(FrameworkUtil.getBundle(getClass())).toPath()//
+				.resolve("config") //$NON-NLS-1$
+				.resolve("log4j2.xml"); //$NON-NLS-1$
 	}
 
 }
