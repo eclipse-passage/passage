b5ff874871281ad28eeb0a3df9ff80adcb2883e9
Elena Parovyshnaia <elena.parovyshnaya@gmail.com>
2021-04-20 13:52:43 +0300

    Bug 572962 invite 'keys' domain
    Declare 'keys' EMF classes to implement RO interfaces
    
    Signed-off-by: eparovyshnaya <elena.parovyshnaya@gmail.com>

---
+++ bundles/org.eclipse.passage.lic.keys/src/org/eclipse/passage/lic/keys/package-info.java
@@ -0,0 +1,17 @@
+/* *****************************************************************************
+ * Copyright (c) 2021 ArSysOp
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0/.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     ArSysOp - initial API and implementation
+ *******************************************************************************/
+/**
+ * Read only interfaces designed to be implemented by generated EMF entities of
+ * {@code keys} domain.
+ */
+package org.eclipse.passage.lic.keys;
