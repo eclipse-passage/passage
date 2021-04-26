ae4211e69d3691471d4ed60c5915c999714b1d5e
Elena Parovyshnaia <elena.parovyshnaya@gmail.com>
2021-04-26 17:30:06 +0300

    Bug 573129 StremCodec must supply encryption keys to streams
    Implement user-home based keys storage (temporarily, until workspace
    starts working)
    
    Signed-off-by: eparovyshnaya <elena.parovyshnaya@gmail.com>

---
+++ bundles/org.eclipse.passage.loc.workspace/src/org/eclipse/passage/loc/workspace/LocalFileHandle.java
@@ -0,0 +1,48 @@
+/*******************************************************************************
+ * Copyright (c) 2021 ArSysOp
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0/.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     ArSysOp - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.passage.loc.workspace;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.nio.file.Path;
+
+import org.eclipse.passage.loc.internal.api.workspace.ResourceHandle;
+
+/**
+ * temporary
+ */
+class LocalFileHandle implements ResourceHandle {
+
+	private final Path path;
+
+	public LocalFileHandle(Path path) {
+		this.path = path;
+	}
+
+	@Override
+	public String info() {
+		return path.toAbsolutePath().toString();
+	}
+
+	@Override
+	public void write(byte[] content) throws Exception {
+		File file = path.toFile();
+		file.getParentFile().mkdirs();
+		try (OutputStream out = new FileOutputStream(file)) {
+			out.write(content);
+			out.flush();
+		}
+	}
+
+}
