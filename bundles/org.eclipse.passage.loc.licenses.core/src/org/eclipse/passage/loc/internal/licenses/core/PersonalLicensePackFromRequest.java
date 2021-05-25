b53ada3e837d99de184018d635ae9702d935da50
Elena Parovyshnaia <elena.parovyshnaya@gmail.com>
2021-05-25 20:46:28 +0300

    Bug 547799 LicensePack should have information about Licensing Operator
    Fix personal license issuing
    
    Signed-off-by: eparovyshnaya <elena.parovyshnaya@gmail.com>

--- bundles/org.eclipse.passage.loc.licenses.core/src/org/eclipse/passage/loc/internal/licenses/core/PersonalLicensePackFromRequest.java
+++ bundles/org.eclipse.passage.loc.licenses.core/src/org/eclipse/passage/loc/internal/licenses/core/PersonalLicensePackFromRequest.java
@@ -12,12 +12,16 @@
  *******************************************************************************/
 package org.eclipse.passage.loc.internal.licenses.core;
 
+import java.util.Date;
 import java.util.function.Supplier;
 
 import org.eclipse.passage.lic.internal.licenses.model.EmptyPersonalLicensePack;
 import org.eclipse.passage.lic.licenses.LicensePlanDescriptor;
 import org.eclipse.passage.lic.licenses.LicensePlanFeatureDescriptor;
 import org.eclipse.passage.lic.licenses.model.api.PersonalLicensePack;
+import org.eclipse.passage.lic.licenses.model.api.ValidityPeriod;
+import org.eclipse.passage.lic.licenses.model.api.ValidityPeriodClosed;
+import org.eclipse.passage.lic.licenses.model.meta.LicensesFactory;
 import org.eclipse.passage.loc.internal.api.PersonalLicenseRequest;
 import org.eclipse.passage.loc.internal.licenses.LicenseRegistry;
 
@@ -41,11 +45,14 @@ final class PersonalLicensePackFromRequest implements Supplier<PersonalLicensePa
 	}
 
 	private void installRequisites(PersonalLicensePack pack) {
+		pack.getLicense().setIdentifier(request.identifier());
+		pack.getLicense().setIssueDate(new Date());
 		pack.getLicense().getUser().setIdentifier(request.user());
 		pack.getLicense().getUser().setName(request.userFullName());
 		pack.getLicense().getProduct().setIdentifier(request.productIdentifier());
 		pack.getLicense().getProduct().setVersion(request.productVersion());
 		pack.getLicense().setPlan(request.plan());
+		pack.getLicense().setValid(valid());
 	}
 
 	private void installGrants(PersonalLicensePack pack) {
@@ -53,6 +60,13 @@ final class PersonalLicensePackFromRequest implements Supplier<PersonalLicensePa
 		for (LicensePlanFeatureDescriptor feature : plan.getFeatures()) {
 			pack.getGrants().add(new LicenseGrantFromRequest(feature, request).get());
 		}
+	}
+
+	private ValidityPeriod valid() {
+		ValidityPeriodClosed valid = LicensesFactory.eINSTANCE.createValidityPeriodClosed();
+		valid.setFrom(request.validFrom());
+		valid.setUntil(request.validUntil());
+		return valid;
 	}
 
 }
