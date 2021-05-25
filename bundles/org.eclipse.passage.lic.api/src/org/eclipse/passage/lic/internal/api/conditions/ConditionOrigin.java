482946a048af2c09631480e66e7449e27e61e5ee
Elena Parovyshnaia <elena.parovyshnaya@gmail.com>
2021-05-25 19:52:57 +0300

    Bug 547799 LicensePack should have information about Licensing Operator
    extend ConditionOrigin with the signature of the begetting license
    
    Signed-off-by: eparovyshnaya <elena.parovyshnaya@gmail.com>

--- bundles/org.eclipse.passage.lic.api/src/org/eclipse/passage/lic/internal/api/conditions/ConditionOrigin.java
+++ bundles/org.eclipse.passage.lic.api/src/org/eclipse/passage/lic/internal/api/conditions/ConditionOrigin.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2020 ArSysOp
+ * Copyright (c) 2020, 2021 ArSysOp
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -17,5 +17,7 @@ public interface ConditionOrigin {
 	ConditionMiningTarget miner();
 
 	String coordinates();
+
+	LicenseSignature signature();
 
 }
--- bundles/org.eclipse.passage.lic.base/src/org/eclipse/passage/lic/internal/base/conditions/BaseConditionOrigin.java
+++ bundles/org.eclipse.passage.lic.base/src/org/eclipse/passage/lic/internal/base/conditions/BaseConditionOrigin.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2020 ArSysOp
+ * Copyright (c) 2020, 2021 ArSysOp
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -13,20 +13,33 @@
 package org.eclipse.passage.lic.internal.base.conditions;
 
 import java.util.Objects;
+import java.util.Optional;
 
 import org.eclipse.passage.lic.internal.api.conditions.ConditionMiningTarget;
 import org.eclipse.passage.lic.internal.api.conditions.ConditionOrigin;
+import org.eclipse.passage.lic.internal.api.conditions.LicenseSignature;
 
 public final class BaseConditionOrigin implements ConditionOrigin {
 
 	private final ConditionMiningTarget miner;
 	private final String coordinates;
+	private final LicenseSignature signature;
 
 	public BaseConditionOrigin(ConditionMiningTarget miner, String coordinates) {
+		this(miner, coordinates, new BaseLicenseSignature());
+	}
+
+	public BaseConditionOrigin(ConditionMiningTarget miner, String coordinates, Optional<LicenseSignature> signature) {
+		this(miner, coordinates, signature.orElse(new BaseLicenseSignature()));
+	}
+
+	public BaseConditionOrigin(ConditionMiningTarget miner, String coordinates, LicenseSignature signature) {
 		Objects.requireNonNull(miner, "BaseConditionOrigin::miner"); //$NON-NLS-1$
 		Objects.requireNonNull(coordinates, "BaseConditionOrigin::coordinates"); //$NON-NLS-1$
+		Objects.requireNonNull(signature, "BaseConditionOrigin::signature"); //$NON-NLS-1$
 		this.miner = miner;
 		this.coordinates = coordinates;
+		this.signature = signature;
 	}
 
 	@Override
@@ -37,6 +50,11 @@ public final class BaseConditionOrigin implements ConditionOrigin {
 	@Override
 	public String coordinates() {
 		return coordinates;
+	}
+
+	@Override
+	public LicenseSignature signature() {
+		return signature;
 	}
 
 }
