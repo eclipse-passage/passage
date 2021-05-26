17831596093bbc1dba654a1d4c55a5f1e0dcaa31
Alexander Fedorov <alexander.fedorov@arsysop.ru>
2021-05-26 08:36:07 +0300

    Bug 571998 - The contribution of Passage to SimRel has not been signed
    Increment version for org.eclipse.passage.loc.edit.ui
    
    Signed-off-by: Alexander Fedorov <alexander.fedorov@arsysop.ru>

--- bundles/org.eclipse.passage.loc.edit.ui/src/org/eclipse/passage/loc/edit/ui/DomainRegistryLabelProvider.java
+++ bundles/org.eclipse.passage.loc.edit.ui/src/org/eclipse/passage/loc/edit/ui/DomainRegistryLabelProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2018, 2020 ArSysOp
+ * Copyright (c) 2018, 2021 ArSysOp
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -21,7 +21,6 @@ import org.eclipse.passage.lic.jface.resource.LicensingImages;
 import org.eclipse.passage.loc.internal.workbench.LocDomainRegistryAccess;
 import org.eclipse.swt.graphics.Image;
 
-//FIXME: rewrite to avoid restriction warnings
 class DomainRegistryLabelProvider extends LabelProvider {
 
 	private final LocDomainRegistryAccess access;
