db7470fbcc109c759192284c68e6d0ed140de021
Elena Parovyshnaia <elena.parovyshnaya@gmail.com>
2021-07-08 18:18:14 +0300

    Bug 574598 Dialog-facilitated import of unreadable license fails
    as first step: cover and properly expose any unexpected failure of any
    subservice
    
    Signed-off-by: eparovyshnaya <elena.parovyshnaya@gmail.com>

--- bundles/org.eclipse.passage.lic.jface/src/org/eclipse/passage/lic/internal/jface/EquinoxPassageUI.java
+++ bundles/org.eclipse.passage.lic.jface/src/org/eclipse/passage/lic/internal/jface/EquinoxPassageUI.java
@@ -21,7 +21,10 @@ import org.eclipse.jface.window.Window;
 import org.eclipse.passage.lic.internal.api.PassageUI;
 import org.eclipse.passage.lic.internal.api.ServiceInvocationResult;
 import org.eclipse.passage.lic.internal.api.access.GrantLockAttempt;
+import org.eclipse.passage.lic.internal.api.diagnostic.Trouble;
+import org.eclipse.passage.lic.internal.api.diagnostic.TroubleCode;
 import org.eclipse.passage.lic.internal.api.restrictions.ExaminationCertificate;
+import org.eclipse.passage.lic.internal.base.BaseServiceInvocationResult;
 import org.eclipse.passage.lic.internal.base.restrictions.CertificateWorthAttention;
 import org.eclipse.passage.lic.internal.equinox.EquinoxPassage;
 import org.eclipse.passage.lic.internal.equinox.EquinoxPassageLicenseCoverage;
@@ -56,8 +59,15 @@ public final class EquinoxPassageUI implements PassageUI {
 			Function<T, ExaminationCertificate> certificate, //
 			Predicate<Optional<ExaminationCertificate>> ok) {
 		ServiceInvocationResult<T> result = service.get();
-		while (exposeAndMayBeEvenFix(result, certificate, ok)) {
-			result = service.get();
+		try {
+			while (exposeAndMayBeEvenFix(result, certificate, ok)) {
+				result = service.get();
+			}
+		} catch (Exception e) {
+			return new BaseServiceInvocationResult<>(new Trouble(//
+					new TroubleCode.Of(-1, "Unexpected error"), //$NON-NLS-1$
+					"One of supplied subservices failed", //$NON-NLS-1$
+					e));
 		}
 		return result;
 	}
