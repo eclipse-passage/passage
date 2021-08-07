3a1dcbf5a28fe4a5aac4c1dd805cd9dae55f5d95
Alexander Fedorov <alexander.fedorov@arsysop.ru>
2021-08-07 21:19:08 +0300

    Bug 575165 - [Passage][Operator] License Agreement management: gui
    Add org.eclipse.passage.loc.agreements.emfforms
    
    Signed-off-by: Alexander Fedorov <alexander.fedorov@arsysop.ru>

---
+++ bundles/org.eclipse.passage.loc.agreements.emfforms/src/org/eclipse/passage/loc/agreements/emfforms/parts/AgreementsCreateElementCallback.java
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Copyright (c) 2021 ArSysOp
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0/.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     ArSysOp - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.passage.loc.agreements.emfforms.parts;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emfforms.spi.swt.treemasterdetail.util.CreateElementCallback;
+
+public class AgreementsCreateElementCallback implements CreateElementCallback {
+
+	@Override
+	public void initElement(EObject parent, EReference reference, EObject newObject) {
+	}
+
+	@Override
+	public boolean beforeCreateElement(Object newElement) {
+		return true;
+	}
+
+	@Override
+	public void afterCreateElement(Object newElement) {
+	}
+
+}
---
+++ bundles/org.eclipse.passage.loc.agreements.emfforms/src/org/eclipse/passage/loc/agreements/emfforms/parts/AgreementsDetailsPart.java
@@ -0,0 +1,46 @@
+/*******************************************************************************
+ * Copyright (c) 2021 ArSysOp
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0/.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     ArSysOp - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.passage.loc.agreements.emfforms.parts;
+
+import javax.inject.Inject;
+
+import org.eclipse.e4.core.contexts.IEclipseContext;
+import org.eclipse.e4.core.di.annotations.Optional;
+import org.eclipse.e4.ui.di.UIEventTopic;
+import org.eclipse.e4.ui.model.application.ui.basic.MPart;
+import org.eclipse.e4.ui.workbench.modeling.ESelectionService;
+import org.eclipse.emfforms.spi.swt.treemasterdetail.util.CreateElementCallback;
+import org.eclipse.passage.lic.agreements.model.api.AgreementsGroup;
+import org.eclipse.passage.loc.internal.agreements.AgreementRegistryEvents;
+import org.eclipse.passage.loc.workbench.emfforms.parts.DetailsView;
+
+public class AgreementsDetailsPart extends DetailsView {
+
+	@Inject
+	public AgreementsDetailsPart(MPart part, ESelectionService selectionService) {
+		super(part, selectionService);
+	}
+
+	@Inject
+	@Optional
+	public void showFeatureSet(@UIEventTopic(AgreementRegistryEvents.AGREEMENTS_GROUP_CREATE) AgreementsGroup input,
+			IEclipseContext context) {
+		show(input, context);
+	}
+
+	@Override
+	protected CreateElementCallback getCreateElementCallback() {
+		return new AgreementsCreateElementCallback();
+	}
+
+}
