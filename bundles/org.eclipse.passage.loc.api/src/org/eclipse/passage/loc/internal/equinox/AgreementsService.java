1cba969999f670fa8c7d80d74f465f1578a578fd
Elena Parovyshnaia <elena.parovyshnaya@gmail.com>
2021-08-14 18:52:54 +0300

    Bug 575166 License Agreement management: license issuing
    extract service supplier for reuse
    
    Signed-off-by: eparovyshnaya <elena.parovyshnaya@gmail.com>

---
+++ bundles/org.eclipse.passage.loc.api/src/org/eclipse/passage/loc/internal/equinox/AgreementsService.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2021 ArSysOp
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0/.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     ArSysOp - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.passage.loc.internal.equinox;
+
+import java.util.Optional;
+
+import org.eclipse.passage.lic.api.LicensingException;
+import org.eclipse.passage.loc.internal.api.workspace.Agreements;
+
+public final class AgreementsService {
+
+	public Agreements get() throws LicensingException {
+		Optional<Agreements> service = new OperatorGearAware()
+				.withGear(gear -> Optional.of(gear.workspace().agreements()));
+		if (!service.isPresent()) {
+			throw new LicensingException("There is no Agreements service supplied by Operator Workspace"); //$NON-NLS-1$
+		}
+		return service.get();
+	}
+
+}
