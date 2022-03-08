ccd97c316eb93291ce79f407322623bfa85f46e0
Elena Parovyshnaia <elena.parovyshnaya@gmail.com>
2022-03-08 13:02:26 +0300

    #1059 Prepare sourcebase for 2.3.0 release
    supply requirement filtering AC configuration for library scenario

---
+++ bundles/org.eclipse.passage.lic.execute/src/org/eclipse/passage/lic/execute/BundleRequirementsForNamespace.java
@@ -0,0 +1,36 @@
+package org.eclipse.passage.lic.execute;
+
+import org.eclipse.passage.lic.api.registry.Registry;
+import org.eclipse.passage.lic.api.registry.StringServiceId;
+import org.eclipse.passage.lic.api.requirements.Requirement;
+import org.eclipse.passage.lic.api.requirements.ResolvedRequirements;
+import org.eclipse.passage.lic.api.requirements.ResolvedRequirementsRegistry;
+import org.eclipse.passage.lic.base.registry.ReadOnlyRegistry;
+import org.eclipse.passage.lic.equinox.requirements.BundleRequirements;
+import org.eclipse.passage.lic.internal.base.requirements.FilteredRequirements;
+
+@SuppressWarnings("restriction")
+final class BundleRequirementsForNamespace implements ResolvedRequirementsRegistry {
+
+	private final String namespace;
+	private final Registry<StringServiceId, ResolvedRequirements> delegate;
+
+	BundleRequirementsForNamespace(String namespace) {
+		this.namespace = namespace;
+		this.delegate = new ReadOnlyRegistry<>(//
+				new FilteredRequirements(//
+						new BundleRequirements(), //
+						this::declaredByAtlas//
+				));
+	}
+
+	@Override
+	public Registry<StringServiceId, ResolvedRequirements> get() {
+		return delegate;
+	}
+
+	private boolean declaredByAtlas(Requirement requirement) {
+		return requirement.feature().identifier().startsWith(namespace);
+	}
+
+}
