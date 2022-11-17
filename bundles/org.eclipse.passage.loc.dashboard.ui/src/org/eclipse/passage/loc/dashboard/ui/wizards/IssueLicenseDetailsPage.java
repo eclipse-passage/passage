d916996dbd13790397d08e484e5904447ba132a0
Elena Parovyshnaia <elena.parovyshnaya@gmail.com>
2022-11-17 11:36:34 +0300

    1147 Operator: get rid of javax.mail dependency (#1149)
    * 1147 Operator: get rid of javax.mail dependency

--- bundles/org.eclipse.passage.loc.dashboard.ui/src/org/eclipse/passage/loc/dashboard/ui/wizards/IssueLicenseDetailsPage.java
+++ bundles/org.eclipse.passage.loc.dashboard.ui/src/org/eclipse/passage/loc/dashboard/ui/wizards/IssueLicenseDetailsPage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019, 2020 ArSysOp
+ * Copyright (c) 2019, 2022 ArSysOp
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -12,37 +12,21 @@
  *******************************************************************************/
 package org.eclipse.passage.loc.dashboard.ui.wizards;
 
-import java.util.Optional;
-import java.util.function.Supplier;
-import java.util.stream.Collectors;
-
-import org.eclipse.e4.core.contexts.IEclipseContext;
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.wizard.WizardPage;
-import org.eclipse.passage.lic.email.Mailing;
-import org.eclipse.passage.lic.licenses.PersonalLicensePackDescriptor;
 import org.eclipse.passage.loc.internal.dashboard.ui.i18n.IssueLicensePageMessages;
-import org.eclipse.passage.loc.internal.licenses.core.EmailTemplate;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Text;
 
-public class IssueLicenseDetailsPage extends WizardPage {
+//TODO: looks empty, fill or kill 
+public final class IssueLicenseDetailsPage extends WizardPage {
 
-	private final IEclipseContext context;
-	private final Supplier<PersonalLicensePackDescriptor> data;
-	private Text info;
-	private Text from;
-	private String mailFrom = ""; //$NON-NLS-1$
-
-	protected IssueLicenseDetailsPage(String pageName, Supplier<PersonalLicensePackDescriptor> data, IEclipseContext context) {
-		super(pageName);
-		this.context = context;
-		this.data = data;
+	protected IssueLicenseDetailsPage(String name) {
+		super(name);
 		setTitle(IssueLicensePageMessages.IssueLicenseDetailsPage_page_title);
 		setDescription(IssueLicensePageMessages.IssueLicenseDetailsPage_page_description);
 	}
@@ -52,43 +36,10 @@ public class IssueLicenseDetailsPage extends WizardPage {
 		Composite composite = new Composite(parent, SWT.NONE);
 		composite.setLayoutData(GridDataFactory.fillDefaults().grab(true, true).create());
 		composite.setLayout(new GridLayout());
-		info = new Text(composite, SWT.BORDER | SWT.MULTI | SWT.WRAP);
+		Text info = new Text(composite, SWT.BORDER | SWT.MULTI | SWT.WRAP);
 		info.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
-		Optional.ofNullable(context//
-				.get(Mailing.class))//
-				.ifPresent(m -> createEmlButton(composite));
 		setControl(composite);
 		Dialog.applyDialogFont(composite);
-	}
-
-	private void createEmlButton(Composite parent) {
-		Group prepareEml = new Group(parent, SWT.CHECK);
-		prepareEml.setLayoutData(new GridData(SWT.FILL, SWT.FILL, false, false));
-		prepareEml.setText(IssueLicensePageMessages.IssueLicenseDetailsPage_lbl_eml_text);
-		prepareEml.setLayout(new GridLayout());
-		from = new Text(prepareEml, SWT.NONE);
-		from.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
-		from.addModifyListener(e -> mailFrom = from.getText().trim());
-	}
-
-	@Override
-	public void setVisible(boolean visible) {
-		super.setVisible(visible);
-		if (visible) {
-			if (info == null || info.isDisposed()) {
-				return;
-			}
-			Mailing mailing = context.get(Mailing.class);
-			if (mailing == null) {
-				return;
-			}
-			info.setText(new EmailTemplate(mailing).details(data.get()).stream()//
-					.collect(Collectors.joining(System.lineSeparator())));
-		}
-	}
-
-	public String mailFrom() {
-		return mailFrom;
 	}
 
 }
--- bundles/org.eclipse.passage.loc.dashboard.ui/src/org/eclipse/passage/loc/dashboard/ui/wizards/IssueLicenseWizard.java
+++ bundles/org.eclipse.passage.loc.dashboard.ui/src/org/eclipse/passage/loc/dashboard/ui/wizards/IssueLicenseWizard.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019, 2021 ArSysOp
+ * Copyright (c) 2019, 2022 ArSysOp
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -12,18 +12,12 @@
  *******************************************************************************/
 package org.eclipse.passage.loc.dashboard.ui.wizards;
 
-import java.io.File;
-import java.io.IOException;
-
 import org.eclipse.e4.core.contexts.IEclipseContext;
 import org.eclipse.e4.core.services.events.IEventBroker;
-import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.wizard.IWizardPage;
 import org.eclipse.jface.wizard.Wizard;
-import org.eclipse.osgi.util.NLS;
 import org.eclipse.passage.lic.api.ServiceInvocationResult;
 import org.eclipse.passage.lic.base.diagnostic.NoSevereErrors;
-import org.eclipse.passage.lic.email.Mailing;
 import org.eclipse.passage.lic.internal.jface.dialogs.licensing.DiagnosticDialog;
 import org.eclipse.passage.lic.licenses.PersonalLicensePackDescriptor;
 import org.eclipse.passage.lic.licenses.model.api.PersonalLicensePack;
@@ -31,12 +25,8 @@ import org.eclipse.passage.loc.dashboard.ui.wizards.license.WizardInfoBar;
 import org.eclipse.passage.loc.internal.api.IssuedLicense;
 import org.eclipse.passage.loc.internal.api.OperatorLicenseService;
 import org.eclipse.passage.loc.internal.dashboard.ui.i18n.IssueLicensePageMessages;
-import org.eclipse.passage.loc.internal.licenses.core.EmailTemplate;
 import org.eclipse.passage.loc.users.ui.UsersUi;
 import org.eclipse.passage.loc.workbench.LocWokbench;
-import org.eclipse.swt.program.Program;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Shell;
 
 public class IssueLicenseWizard extends Wizard {
 
@@ -58,7 +48,7 @@ public class IssueLicenseWizard extends Wizard {
 		addPage(request.get());
 		pack = new IssueLicensePackPage(IssueLicensePackPage.class.getName(), request::request, context);
 		addPage(pack);
-		info = new IssueLicenseDetailsPage(IssueLicenseDetailsPage.class.getName(), pack::pack, context);
+		info = new IssueLicenseDetailsPage(IssueLicenseDetailsPage.class.getName());
 		addPage(info);
 	}
 
@@ -83,28 +73,8 @@ public class IssueLicenseWizard extends Wizard {
 		} else {
 			new WizardInfoBar(this).wipe();
 			new LicenseIssuedNotification(getShell()).showPersonal(result.data().get());
-			String mailFrom = info.mailFrom();
-			if (!mailFrom.isEmpty()) {
-				processingMail(mailFrom, licensePack, result.data().get());
-			}
 			broadcast(result.data().get());
 			return true;
-		}
-	}
-
-	private void processingMail(String from, PersonalLicensePackDescriptor licensePack, IssuedLicense result) {
-		Mailing mailing = context.get(Mailing.class);
-		EmailTemplate template = new EmailTemplate(mailing);
-		try {
-			File eml = template.createEmlFile(from, licensePack, result);
-			String msg = NLS.bind(IssueLicensePageMessages.IssueLicenseMailRequestDialog_text, eml.getAbsolutePath());
-			Display.getDefault().asyncExec(() -> MessageDialog.openInformation(context.get(Shell.class),
-					IssueLicensePageMessages.IssueLicenseMailRequestDialog_title, msg));
-			Program.launch(eml.getAbsolutePath());
-		} catch (IOException e) {
-			Display.getDefault().asyncExec(() -> MessageDialog.openError(context.get(Shell.class),
-					IssueLicensePageMessages.IssueLicenseMailRequestDialog_title, e.getMessage()));
-			Program.launch(template.mailTo(licensePack));
 		}
 	}
 
