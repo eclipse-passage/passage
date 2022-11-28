21a0f3fb55900b089c18b2ca4d94feacfbea4923
eparovyshnaya <elena.parovyshnaya@gmail.com>
2022-11-28 21:39:59 +0300

    last page of a license issuing wizard is always empty #1148
    add description to floating license issue wizard's info
    
    Signed-off-by: eparovyshnaya <elena.parovyshnaya@gmail.com>

---
+++ bundles/org.eclipse.passage.lic.licenses.model/src/org/eclipse/passage/lic/licenses/model/described/DescribedFloatingLicense.java
@@ -0,0 +1,37 @@
+/*******************************************************************************
+ * Copyright (c) 2022 ArSysOp
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0/.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     ArSysOp - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.passage.lic.licenses.model.described;
+
+import java.util.function.Supplier;
+
+import org.eclipse.passage.lic.licenses.FloatingLicensePackDescriptor;
+
+public final class DescribedFloatingLicense implements Supplier<String> {
+
+	private final FloatingLicensePackDescriptor pack;
+
+	public DescribedFloatingLicense(FloatingLicensePackDescriptor license) {
+		this.pack = license;
+	}
+
+	@Override
+	public String get() {
+		StringBuilder out = new StringBuilder();
+		out.append(new DescribedLicenseRequisites(pack.getLicense()).get());
+		out.append(new DescribedHost(pack.getHost()).get());
+		out.append(new DescribedUserGrants(pack.getUsers()).get());
+		out.append(new DescribedFloatingFeatureGrants(pack.getFeatures()).get());
+		return out.toString();
+	}
+
+}
---
+++ bundles/org.eclipse.passage.lic.licenses.model/src/org/eclipse/passage/lic/licenses/model/described/DescribedUserGrant.java
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ * Copyright (c) 2022 ArSysOp
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0/.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     ArSysOp - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.passage.lic.licenses.model.described;
+
+import org.eclipse.passage.lic.internal.licenses.model.i18n.Messages;
+import org.eclipse.passage.lic.licenses.UserGrantDescriptor;
+
+public final class DescribedUserGrant extends Described {
+
+	private UserGrantDescriptor grant;
+
+	public DescribedUserGrant(UserGrantDescriptor grant) {
+		this.grant = grant;
+	}
+
+	@Override
+	public String get() {
+		StringBuilder out = new StringBuilder();
+		out.append(tab).append(Messages.getString("DescribedPersonalFeatureGrant_user")) //$NON-NLS-1$
+				.append(grant.getUser()).append(nl)//
+				.append(tabs).append(new DescribedAuthenticationInstructions(grant.getAuthentication()).get())
+				.append(nl);
+		return out.toString();
+	}
+
+}
