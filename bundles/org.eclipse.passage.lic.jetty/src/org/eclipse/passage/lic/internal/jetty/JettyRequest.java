f08e2ed0aa7d698a1a87cf9284006fad2cdd6dc0
Alexander Fedorov <alexander.fedorov@arsysop.ru>
2023-11-15 11:48:42 +0300

    [#1251] upgrade to Jetty 12
    Initial signature adaptation, payload is not yet considered
    
    Signed-off-by: Alexander Fedorov <alexander.fedorov@arsysop.ru>

--- bundles/org.eclipse.passage.lic.jetty/src/org/eclipse/passage/lic/internal/jetty/JettyRequest.java
+++ bundles/org.eclipse.passage.lic.jetty/src/org/eclipse/passage/lic/internal/jetty/JettyRequest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2021 ArSysOp
+ * Copyright (c) 2021, 2023 ArSysOp
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -16,31 +16,37 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Objects;
 
-import javax.servlet.http.HttpServletRequest;
-
+import org.eclipse.jetty.http.HttpField;
+import org.eclipse.jetty.http.HttpHeader;
+import org.eclipse.jetty.server.Request;
 import org.eclipse.passage.lic.internal.net.api.handle.NetRequest;
 
 public final class JettyRequest implements NetRequest {
 
-	private final HttpServletRequest origin;
+	private final Request origin;
 
-	public JettyRequest(HttpServletRequest origin) {
+	public JettyRequest(Request origin) {
 		Objects.requireNonNull(origin, "JettyRequest::origin"); //$NON-NLS-1$
 		this.origin = origin;
 	}
 
 	@Override
 	public String parameter(String name) {
-		return origin.getParameter(name);
+		//FIXME: AF: most probably this is it
+		return Request.extractQueryParameters(origin).getValue(name);
 	}
 
 	@Override
 	public byte[] content() throws IOException {
-		byte[] content = new byte[origin.getContentLength()];
-		try (InputStream stream = origin.getInputStream()) {
+		//FIXME: AF: field can be null
+		HttpField field = origin.getHeaders().getField(HttpHeader.CONTENT_LENGTH);
+		//FIXME: AF: they expect long here
+		long length = field.getLongValue();
+		byte[] content = new byte[(int) length];
+		try (InputStream stream = Request.asInputStream(origin)) {
 			stream.read(content);
 		}
 		return content;
 	}
-
+	
 }
