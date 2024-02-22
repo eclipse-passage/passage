6c4f6f4595022354e3436e4a6bac93eee6550aae
eparovyshnaya <elena.parovyshnaya@gmail.com>
2024-02-22 13:58:37 +0300

    [#1292] Operator: contribute editing domain for a registry
    create/update license headers

--- bundles/org.eclipse.passage.loc.api/src/org/eclipse/passage/loc/internal/api/OperatorGear.java
+++ bundles/org.eclipse.passage.loc.api/src/org/eclipse/passage/loc/internal/api/OperatorGear.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2021 ArSysOp
+ * Copyright (c) 2021, 2024 ArSysOp
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -9,6 +9,7 @@
  *
  * Contributors:
  *     ArSysOp - initial API and implementation
+ *     ArSysOp - further evolution
  *******************************************************************************/
 package org.eclipse.passage.loc.internal.api;
 
