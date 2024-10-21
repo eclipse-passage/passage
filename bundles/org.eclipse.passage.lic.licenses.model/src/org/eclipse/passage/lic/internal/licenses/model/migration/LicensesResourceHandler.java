2639cae5f5a68d502119295d827bc4eeb03db0a8
Elena Parovyshnaia <elena.parovyshnaya@gmail.com>
2024-10-21 14:04:18 +0300

    1441 model (#1452)
    * [#1441] Prepare 4.0.0 M1
    
    bump up licenses meta-model version
    
    * [#1441] Prepare 4.0.0 M1
    
    bump up licenses meta-model version
    
    * [#1441] Prepare 4.0.0 M1
    
    bump up licenses meta-model version

--- bundles/org.eclipse.passage.lic.licenses.model/src/org/eclipse/passage/lic/internal/licenses/model/migration/LicensesResourceHandler.java
+++ bundles/org.eclipse.passage.lic.licenses.model/src/org/eclipse/passage/lic/internal/licenses/model/migration/LicensesResourceHandler.java
@@ -44,6 +44,7 @@ public class LicensesResourceHandler extends MigratingResourceHandler {
 		migrate100();
 		migrate110();
 		migrate200();
+		migrate300();
 	}
 
 	@Override
@@ -142,6 +143,12 @@ public class LicensesResourceHandler extends MigratingResourceHandler {
 
 	private void migrate200() {
 		String uri = "http://www.eclipse.org/passage/lic/licenses/2.0.0"; //$NON-NLS-1$
+		LicensesPackage delegate = LicensesPackage.eINSTANCE;
+		EPackage.Registry.INSTANCE.computeIfAbsent(uri, ns -> delegate);
+	}
+
+	private void migrate300() {
+		String uri = "http://www.eclipse.org/passage/lic/licenses/3.0.0"; //$NON-NLS-1$
 		LicensesPackage delegate = LicensesPackage.eINSTANCE;
 		EPackage.Registry.INSTANCE.computeIfAbsent(uri, ns -> delegate);
 	}
