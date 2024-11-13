ab755c3e74d1619719faabb6c927e8e53f28741b
Alexander Fedorov <alexander.fedorov@arsysop.ru>
2024-11-13 21:22:09 +0300

    [#1456] Correct type names for registry contract tests
    `KeyKeepersRegitryTest` ->
    `KeyKeepersRegistryTest`

--- tests/org.eclipse.passage.lic.api.tests/src/org/eclipse/passage/lic/api/tests/KeyKeepersRegistryTest.java
+++ tests/org.eclipse.passage.lic.api.tests/src/org/eclipse/passage/lic/api/tests/KeyKeepersRegistryTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2020, 2021 ArSysOp
+ * Copyright (c) 2020, 2024 ArSysOp
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -9,6 +9,7 @@
  *
  * Contributors:
  *     ArSysOp - initial API and implementation
+ *     ArSysOp - further support and improvements
  *******************************************************************************/
 package org.eclipse.passage.lic.api.tests;
 
@@ -30,7 +31,7 @@ import org.eclipse.passage.lic.api.tests.fakes.io.FakeKeyKeeper;
  * and satisfy all the demands.
  * </p>
  */
-public abstract class KeyKeepersRegitryTest extends ReadOnlyCollectionTest<KeyKeeper> {
+public abstract class KeyKeepersRegistryTest extends ReadOnlyCollectionTest<KeyKeeper> {
 
 	@Override
 	protected final Supplier<Collection<KeyKeeper>> collection() {
