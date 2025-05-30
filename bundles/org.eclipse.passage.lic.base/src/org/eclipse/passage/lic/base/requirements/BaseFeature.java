e7244efe2991be00d3c22482975234c1f620552d
eparovyshnaya <elena.parovyshnaya@gmail.com>
2025-05-30 20:20:17 +0300

    [#1459]  Add contract test for FeatureIdentifier

--- bundles/org.eclipse.passage.lic.base/src/org/eclipse/passage/lic/base/requirements/BaseFeature.java
+++ bundles/org.eclipse.passage.lic.base/src/org/eclipse/passage/lic/base/requirements/BaseFeature.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2020, 2024 ArSysOp
+ * Copyright (c) 2020, 2025 ArSysOp
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
